7fbd11d1815e1565fb7aa3cb34b1e8759adbcfbd
sinev-valentine <sinev-valentine@yandex.ru>
2021-05-13 22:42:29 +0300

    neon-cli adapt impl

--- proxy/plugin/solana_rest_api.py
+++ proxy/plugin/solana_rest_api.py
@@ -23,8 +23,8 @@ from sha3 import keccak_256
 import base58
 import traceback
 import threading
-from .solana_rest_api_tools import EthereumAddress, create_program_address, create_storage_account, evm_loader_id, getLamports, \
-    getAccountInfo,  deploy, transaction_history, solana_cli, solana_url, call_signed, call_emulated, \
+from .solana_rest_api_tools import EthereumAddress,  create_storage_account, evm_loader_id, getLamports, \
+    getAccountInfo, solana_cli, call_signed, call_emulated, \
     Trx, deploy_contract, EthereumError
 from web3 import Web3
 import logging
@@ -41,8 +41,7 @@ modelInstance = None
 class EthereumModel:
     def __init__(self):
         # Initialize user account
-        cli = solana_cli(solana_url)
-        res = cli.call("config get")
+        res = solana_cli().call("config get")
         res = res.splitlines()[-1]
         substr = "Keypair Path: "
         if not res.startswith(substr):
@@ -472,8 +471,8 @@ class SolanaProxyPlugin(HttpWebServerBasePlugin):
                     b'Access-Control-Max-Age': b'86400'
                 })))
             return
-        
-        print('headers', request.headers)
+
+        # print('headers', request.headers)
         logger.debug('<<< %s 0x%x %s', threading.get_ident(), id(self.model), request.body.decode('utf8'))
         response = None
 
