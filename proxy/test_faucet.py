18a4081c672f583ba60fb2e51e734cc664825b85
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2021-08-27 11:47:58 +0300

    Add faucet service to docker-compose-test.yml (166-faucet)

--- proxy/test_faucet.py
+++ proxy/test_faucet.py
@@ -5,12 +5,15 @@ class Test_Faucet(unittest.TestCase):
     def setUpClass(cls):
         print("\n\nTest Faucet Started")
 
+    def test_env(self):
+        print("\n# test_env")
+
+    def test_config(self):
+        print("\n# test_config")
+
     @classmethod
     def tearDownClass(cls):
         print("\nTest Faucet Finished")
-
-    def test_env(self):
-        print("# test_env")
 
 if __name__ == '__main__':
     unittest.main()
