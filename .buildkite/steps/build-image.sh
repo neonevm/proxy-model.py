5bc8937c716f38ca4411d606452c22a55a9af15c
Vasiliy Zaznobin <vasiliy.zaznobin@cyber-core.dev>
2021-09-29 21:38:03 +0300

    Check 5d569396675658f4e67257c8cb41d749b570786c

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 set ${SOLANA_REVISION:=v1.7.9-resources}
-set ${EVM_LOADER_REVISION:=b87f06cc02d45f22428b7aae550a5775fb8e13f7} # TODO: revert to :=latest}
+set ${EVM_LOADER_REVISION:=5d569396675658f4e67257c8cb41d749b570786c} # TODO: revert to :=latest}
 
 # Refreshing neonlabsorg/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull neonlabsorg/solana:${SOLANA_REVISION}
