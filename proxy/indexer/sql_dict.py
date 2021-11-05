eab872b539933390d6fa0c279d2dfae3d3308c5f
Dmitriy Borisenko <otselnik@yandex.ru>
2021-11-05 12:06:49 +0300

    #243 update sqldict

--- proxy/indexer/sql_dict.py
+++ proxy/indexer/sql_dict.py
@@ -1,33 +1,66 @@
 import psycopg
+import os
+
+POSTGRES_DB = os.environ.get("POSTGRES_DB", "neon-db")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "neon-proxy")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "neon-proxy")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+
+try:
+    from cPickle import dumps, loads, HIGHEST_PROTOCOL as PICKLE_PROTOCOL
+except ImportError:
+    from pickle import dumps, loads, HIGHEST_PROTOCOL as PICKLE_PROTOCOL
+
+
+def encode(obj):
+    """Serialize an object using pickle to a binary format accepted by SQLite."""
+    return psycopg.Binary(dumps(obj, protocol=PICKLE_PROTOCOL))
+
+
+def decode(obj):
+    """Deserialize objects retrieved from SQLite."""
+    return loads(bytes(obj))
 
 
 class SQLDict(dict):
-    def __init__(self, filename=None):
-        self.conn = psycopg.connect(dbname='database', user='db_user', password='mypassword', host='localhost')
-        self.conn.execute("CREATE TABLE IF NOT EXISTS kv (key text unique, value text)")
+    def __init__(self, tablename='table', dbname=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, encode=encode, decode=decode):
+        self.encode = encode
+        self.decode = decode
+        self.tablename = tablename
+        self.conn = psycopg.connect(dbname, user, password, host)
+        self.conn.execute("""
+        CREATE TABLE IF NOT EXISTS
+        {} (
+            key TEXT UNIQUE,
+            value BLOB
+        )""".format(self.tablename))
 
     def close(self):
         self.conn.commit()
         self.conn.close()
 
     def __len__(self):
-        rows = self.conn.execute('SELECT COUNT(*) FROM kv').fetchone()[0]
+        REQUEST = 'SELECT COUNT(*) FROM {}'.format(self.tablename)
+        rows = self.conn.execute(REQUEST).fetchone()[0]
         return rows if rows is not None else 0
 
     def iterkeys(self):
+        REQUEST = 'SELECT key FROM {}'.format(self.tablename)
         c = self.conn.cursor()
-        for row in self.conn.execute('SELECT key FROM kv'):
-            yield row[0]
+        for key in c.execute(REQUEST):
+            yield key[0]
 
     def itervalues(self):
+        REQUEST = 'SELECT value FROM {}'.format(self.tablename)
         c = self.conn.cursor()
-        for row in c.execute('SELECT value FROM kv'):
-            yield row[0]
+        for value in c.execute(REQUEST):
+            yield self.decode(value[0])
 
     def iteritems(self):
+        REQUEST = 'SELECT key, value FROM {}'.format(self.tablename)
         c = self.conn.cursor()
-        for row in c.execute('SELECT key, value FROM kv'):
-            yield row[0], row[1]
+        for key, value in c.execute(REQUEST):
+            yield key, self.decode(value)
 
     def keys(self):
         return list(self.iterkeys())
@@ -39,21 +72,25 @@ class SQLDict(dict):
         return list(self.iteritems())
 
     def __contains__(self, key):
-        return self.conn.execute('SELECT 1 FROM kv WHERE key = ?', (key,)).fetchone() is not None
+        REQUEST = 'SELECT 1 FROM {} WHERE key = ?'.format(self.tablename)
+        return self.conn.execute(REQUEST, (key,)).fetchone() is not None
 
     def __getitem__(self, key):
-        item = self.conn.execute('SELECT value FROM kv WHERE key = ?', (key,)).fetchone()
+        REQUEST = 'SELECT value FROM {} WHERE key = ?'.format(self.tablename)
+        item = self.conn.execute(REQUEST, (key,)).fetchone()
         if item is None:
             raise KeyError(key)
-        return item[0]
+        return self.decode(item[0])
 
     def __setitem__(self, key, value):
-        self.conn.execute('REPLACE INTO kv (key, value) VALUES (?,?)', (key, value))
+        REQUEST = 'REPLACE INTO {} (key, value) VALUES (?,?)'.format(self.tablename)
+        self.conn.execute(REQUEST, (key, self.encode(value)))
 
     def __delitem__(self, key):
+        REQUEST = 'DELETE FROM {} WHERE key = ?'.format(self.tablename)
         if key not in self:
             raise KeyError(key)
-        self.conn.execute('DELETE FROM kv WHERE key = ?', (key,))
+        self.conn.execute(REQUEST, (key,))
 
     def __iter__(self):
         return self.iterkeys()
--- proxy/run-proxy.sh
+++ proxy/run-proxy.sh
@@ -16,7 +16,7 @@ if [ "$CONFIG" == "ci" ]; then
   [[ -z "$USE_COMBINED_START_CONTINUE"  ]] && export USE_COMBINED_START_CONTINUE="YES"
   [[ -z "$CONTINUE_COUNT_FACTOR"        ]] && export CONTINUE_COUNT_FACTOR="3"
   [[ -z "$MINIMAL_GAS_PRICE"            ]] && export MINIMAL_GAS_PRICE=0
-  [[ -z "$JOURNAL_MODE"                 ]] && export JOURNAL_MODE="OFF"
+  [[ -z "$POSTGRES_HOST"                ]] && export POSTGRES_HOST="postgres"
 elif [ "$CONFIG" == "local" ]; then
   [[ -z "$SOLANA_URL"                   ]] && export SOLANA_URL="http://localhost:8899"
   [[ -z "$EVM_LOADER"                   ]] && export EVM_LOADER=deploy
@@ -27,7 +27,7 @@ elif [ "$CONFIG" == "local" ]; then
   [[ -z "$EXTRA_GAS"                    ]] && export EXTRA_GAS=0
   [[ -z "$NEON_CLI_TIMEOUT"             ]] && export NEON_CLI_TIMEOUT="0.9"
   [[ -z "$MINIMAL_GAS_PRICE"            ]] && export MINIMAL_GAS_PRICE=0
-  [[ -z "$JOURNAL_MODE"                 ]] && export JOURNAL_MODE="OFF"
+  [[ -z "$POSTGRES_HOST"                ]] && export POSTGRES_HOST="localhost"
 elif [ "$CONFIG" == "devnet" ]; then
   [[ -z "$SOLANA_URL"                   ]] && export SOLANA_URL="https://api.devnet.solana.com"
   [[ -z "$EVM_LOADER"                   ]] && export EVM_LOADER=eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU
@@ -38,7 +38,7 @@ elif [ "$CONFIG" == "devnet" ]; then
   [[ -z "$EXTRA_GAS"                    ]] && export EXTRA_GAS=90000
   [[ -z "$NEON_CLI_TIMEOUT"             ]] && export NEON_CLI_TIMEOUT="10"
   [[ -z "$MINIMAL_GAS_PRICE"            ]] && export MINIMAL_GAS_PRICE=1
-  [[ -z "$JOURNAL_MODE"                 ]] && export JOURNAL_MODE="WAL"
+  [[ -z "$POSTGRES_HOST"                ]] && export POSTGRES_HOST="localhost"
 elif [ "$CONFIG" == "testnet" ]; then
   [[ -z "$SOLANA_URL"                   ]] && export SOLANA_URL="https://api.testnet.solana.com"
   [[ -z "$EVM_LOADER"                   ]] && export EVM_LOADER=eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU
@@ -49,7 +49,7 @@ elif [ "$CONFIG" == "testnet" ]; then
   [[ -z "$EXTRA_GAS"                    ]] && export EXTRA_GAS=90000
   [[ -z "$NEON_CLI_TIMEOUT"             ]] && export NEON_CLI_TIMEOUT="15"
   [[ -z "$MINIMAL_GAS_PRICE"            ]] && export MINIMAL_GAS_PRICE="1"
-  [[ -z "$JOURNAL_MODE"                 ]] && export JOURNAL_MODE="WAL"
+  [[ -z "$POSTGRES_HOST"                ]] && export POSTGRES_HOST="localhost"
 elif [ "$CONFIG" != "custom" ]; then
   exit 1
 fi
