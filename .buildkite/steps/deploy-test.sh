acf4176ec78202b29368f227d316229fe9014500
ivanl <ivanl@cqg.com>
2021-11-11 12:18:22 +0300

    switch to new evm_loader deployment

--- .buildkite/steps/deploy-test.sh
+++ .buildkite/steps/deploy-test.sh
@@ -40,6 +40,7 @@ docker-compose -f proxy/docker-compose-test.yml up -d
 function cleanup_docker {
     docker logs proxy >proxy.log 2>&1
     docker logs solana >solana.log 2>&1
+    docker logs evm_loader >evm_loader.log 2>&1
     grep 'get_measurements' <proxy.log >measurements.log
     echo "Cleanup docker-compose..."
     docker-compose -f proxy/docker-compose-test.yml down --rmi 'all'
