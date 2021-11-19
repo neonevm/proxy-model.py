8c480394d6822403be34da8f0c1dac0fc03d356b
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-11-19 18:03:40 +0500

    Spit and polish

--- proxy/plugin/solana_rest_api_tools.py
+++ proxy/plugin/solana_rest_api_tools.py
@@ -1259,8 +1259,8 @@ def make_instruction_data_from_tx(instruction, private_key=None):
 
 
 def is_account_exists(client: SolanaClient, eth_account: EthereumAddress) -> bool:
-    caller_pda_account, nonce = ether2program(eth_account)
-    info = client.get_account_info(caller_pda_account, commitment=Confirmed)
+    pda_account, nonce = ether2program(eth_account)
+    info = client.get_account_info(pda_account, commitment=Confirmed)
     value = get_from_dict(info, "result", "value")
     return value is not None
 
