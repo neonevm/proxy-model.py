f8c47a43ff42586fa78cf2b772755b8e06d5e439
gigimon <gigimon4ik@gmail.com>
2021-12-01 14:05:39 +0300

    small debug

--- proxy/deploy-test.sh
+++ proxy/deploy-test.sh
@@ -6,6 +6,7 @@ echo "Deploy test..."
 solana config set -u $SOLANA_URL
 solana address || solana-keygen new --no-passphrase
 export $(/spl/bin/neon-cli --commitment confirmed --url $SOLANA_URL --evm_loader "$EVM_LOADER" neon-elf-params)
+export NEON_CLI_TIMEOUT=1
 
 curl -v --header "Content-Type: application/json" --data '{"method":"eth_blockNumber","id":1,"jsonrpc":"2.0","params":[]}' $PROXY_URL
 
