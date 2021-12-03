e8c35b38da84959376ebe7c518f4acbfd48dd9bf
Vasiliy Zaznobin <vz@neonlabs.org>
2021-12-03 13:12:44 +0300

    Add the request['method'] only in debug output, but in a responce

--- proxy/plugin/solana_rest_api.py
+++ proxy/plugin/solana_rest_api.py
@@ -603,7 +603,7 @@ class SolanaProxyPlugin(HttpWebServerBasePlugin):
             traceback.print_exc()
             response['error'] = {'code': -32000, 'message': str(err)}
 
-        response['method'] = request['method']
+        # response['method'] = request['method']
 
         return response
 
@@ -639,7 +639,8 @@ class SolanaProxyPlugin(HttpWebServerBasePlugin):
             traceback.print_exc()
             response = {'jsonrpc': '2.0', 'error': {'code': -32000, 'message': str(err)}}
 
-        logger.debug('>>> %s 0x%0x %s', threading.get_ident(), id(self.model), json.dumps(response))
+        logger.debug('>>> %s 0x%0x %s %s', threading.get_ident(), id(self.model), json.dumps(response),
+                     request['method'])
 
         self.client.queue(memoryview(build_http_response(
             httpStatusCodes.OK, body=json.dumps(response).encode('utf8'),
