3a07c1b2730ccda316d9a41c3fd11a6355ed5256
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-15 22:33:48 +0500

    Rollback some

--- proxy/common_neon/emulator_interactor.py
+++ proxy/common_neon/emulator_interactor.py
@@ -24,13 +24,12 @@ def call_emulated(contract_id, caller_id, data=None, value=None):
 
 def raise_eth_err_by_revert(result_value: str):
     if len(result_value) < 8 or result_value[:8] != '08c379a0':
-        raise EthereumError(code=3, message='Transaction reverted')
-
+        raise EthereumError(code=3, message='execution reverted')
+    
     offset = int(result_value[8:8 + 64], 16)
     length = int(result_value[8 + 64:8 + 64 + 64], 16)
     message = str(bytes.fromhex(result_value[8 + offset * 2 + 64:8 + offset * 2 + 64 + length * 2]), 'utf8')
-    raise EthereumError(code=3, message="Transaction reverted with " + message + ".", data='0x' + result_value)
-
+    raise EthereumError(code=3, message='execution reverted: ' + message, data='0x' + result_value)
 
 def emulator(contract, sender, data, value):
     data = data or "none"
