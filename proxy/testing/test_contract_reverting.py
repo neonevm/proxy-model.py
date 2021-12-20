174ec4737100df752dfeacfac876f0774ffff82e
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-20 22:17:07 +0500

    Fix tests

--- proxy/testing/test_contract_reverting.py
+++ proxy/testing/test_contract_reverting.py
@@ -25,9 +25,9 @@ class TestContractReverting(unittest.TestCase):
 
     def test_revert_message_decoding(self):
         revert_message = decode_revert_message(self._STRING_BASED_REVERT_DATA)
-        self.assertEqual(revert_message, "execution reverted: Not enough Ether provided.")
+        self.assertEqual(revert_message, "Not enough Ether provided.")
 
-    _STRING_BASED_REVERT_DATA = "0x08c379a0" \
+    _STRING_BASED_REVERT_DATA = "08c379a0" \
                                 "0000000000000000000000000000000000000000000000000000000000000020" \
                                 "000000000000000000000000000000000000000000000000000000000000001a" \
                                 "4e6f7420656e6f7567682045746865722070726f76696465642e000000000000"
