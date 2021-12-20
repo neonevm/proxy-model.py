967d79f5acf8b5be97231add45a6168c6a1cdbf0
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-20 22:03:55 +0500

    new test

--- proxy/testing/test_contract_reverting.py
+++ proxy/testing/test_contract_reverting.py
@@ -7,6 +7,7 @@ from solana.rpc.api import Client as SolanaClient
 from eth_account.account import LocalAccount
 
 from .testing_helpers import SolidityContractDeployer
+from ..common_neon.emulator_interactor import decode_revert_message
 
 
 class TestContractReverting(unittest.TestCase):
@@ -21,6 +22,15 @@ class TestContractReverting(unittest.TestCase):
 
         solana_url = os.environ.get("SOLANA_URL", "http://localhost:8899")
         cls._solana_client = SolanaClient(solana_url)
+
+    def test_revert_message_decoding(self):
+        revert_message = decode_revert_message(self._STRING_BASED_REVERT_DATA)
+        self.assertEqual(revert_message, "execution reverted: Not enough Ether provided.")
+
+    _STRING_BASED_REVERT_DATA = "0x08c379a0" \
+                                "0000000000000000000000000000000000000000000000000000000000000020" \
+                                "000000000000000000000000000000000000000000000000000000000000001a" \
+                                "4e6f7420656e6f7567682045746865722070726f76696465642e000000000000"
 
     def test_constructor_raises_string_based_error(self):
         compiled_info = self._contract_deployer.compile_contract(self._CONTRACT_CONSTRUCTOR_STRING_BASED_REVERT)
