1aca196d61c0386ea6318f94cd43cbaa4f3cea66
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-21 17:11:29 +0500

    Add an extra checks on decode revert message

--- proxy/common_neon/emulator_interactor.py
+++ proxy/common_neon/emulator_interactor.py
@@ -31,10 +31,28 @@ def check_emulated_exit_status(result: Dict[str, Any]):
 
 
 def decode_revert_message(data) -> Optional[str]:
-    if len(data) < 8 or data[:8] != '08c379a0':
+    if len(data) == 0:
+        logger.debug(f"Empty reverting signature: {len(data)}, data: 0x{data.hex()}")
         return None
+
+    if len(data) < 8:
+        raise Exception(f"To less bytes to decode reverting signature: {len(data)}, data: 0x{data.hex()}")
+
+    if data[:8] != '08c379a0':
+        logger.debug(f"Failed to decode revert_message, unknown revert signature: {data[:8]}")
+        return None
+
+    if len(data) < 8 + 64:
+        raise Exception(f"Too less bytes to decode revert msg offset: {len(data)}, data: 0x{data.hex()}")
     offset = int(data[8:8 + 64], 16)
-    length = int(data[8 + 64:8 + 64 + 64], 16)
+
+    if len(data) < 8 + offset * 2 + 64:
+        raise Exception(f"Too less bytes to decode revert msg len: {len(data)}, data: 0x{data.hex()}")
+    length = int(data[8 + offset * 2:8 + offset * 2 + 64], 16)
+
+    if len(data) < 8 + offset * 2 + 64 + length * 2:
+        raise Exception(f"Too less bytes to decode revert msg: {len(data)}, data: 0x{data.hex()}")
+
     message = str(bytes.fromhex(data[8 + offset * 2 + 64:8 + offset * 2 + 64 + length * 2]), 'utf8')
     return message
 
