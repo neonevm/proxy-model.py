c4bd1c34932dfb640f229ecf2744bd8ff331b112
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-22 10:25:24 +0500

    spit and polish

--- proxy/common_neon/emulator_interactor.py
+++ proxy/common_neon/emulator_interactor.py
@@ -29,32 +29,32 @@ def check_emulated_exit_status(result: Dict[str, Any]):
             raise EthereumError(code=3, message='execution reverted: ' + result_value, data='0x' + result_value)
 
     if exit_status != "succeed":
-        logger.debug(f"Got emulate exit_status: {exit_status}")
+        logger.debug(f"Got not succeed emulate exit_status: {exit_status}")
         raise Exception("evm emulator error ", result)
 
 
 def decode_revert_message(data) -> Optional[str]:
-    if len(data) == 0:
-        logger.debug(f"Empty revert data")
+    data_len = len(data)
+    if data_len == 0:
         return None
 
-    if len(data) < 8:
-        raise Exception(f"To less bytes to decode revert signature: {len(data)}, data: 0x{data}")
+    if data_len < 8:
+        raise Exception(f"Too less bytes to decode revert signature: {data_len}, data: 0x{data}")
 
     if data[:8] != '08c379a0':
         logger.debug(f"Failed to decode revert_message, unknown revert signature: {data[:8]}")
         return None
 
-    if len(data) < 8 + 64:
-        raise Exception(f"Too less bytes to decode revert msg offset: {len(data)}, data: 0x{data}")
+    if data_len < 8 + 64:
+        raise Exception(f"Too less bytes to decode revert msg offset: {data_len}, data: 0x{data}")
     offset = int(data[8:8 + 64], 16)
 
-    if len(data) < 8 + offset * 2 + 64:
-        raise Exception(f"Too less bytes to decode revert msg len: {len(data)}, data: 0x{data}")
+    if data_len < 8 + offset * 2 + 64:
+        raise Exception(f"Too less bytes to decode revert msg len: {data_len}, data: 0x{data}")
     length = int(data[8 + offset * 2:8 + offset * 2 + 64], 16)
 
-    if len(data) < 8 + offset * 2 + 64 + length * 2:
-        raise Exception(f"Too less bytes to decode revert msg: {len(data)}, data: 0x{data}")
+    if data_len < 8 + offset * 2 + 64 + length * 2:
+        raise Exception(f"Too less bytes to decode revert msg: {data_len}, data: 0x{data}")
 
     message = str(bytes.fromhex(data[8 + offset * 2 + 64:8 + offset * 2 + 64 + length * 2]), 'utf8')
     return message
