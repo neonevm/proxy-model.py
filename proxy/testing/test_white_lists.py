dcfa501f612cf585c756aad4dc6debffac56c82d
Ivan Loboda <lobodatell@yandex.ru>
2022-01-25 19:01:10 +0300

    deploy storage contract

--- proxy/testing/test_white_lists.py
+++ proxy/testing/test_white_lists.py
@@ -11,7 +11,35 @@ from solana.rpc.api import Client as SolanaClient
 from solana.rpc.commitment import Confirmed
 from solana.rpc.types import TxOpts
 from web3 import Web3
+from solcx import install_solc
 
+install_solc(version='0.7.0')
+from solcx import compile_source
+
+STORAGE_SOLIDITY_SOURCE_147 = '''
+pragma solidity >=0.7.0 <0.9.0;
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+    uint256 number;
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+    /**
+     * @dev Return value
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
+'''
 
 class TestWhiteLists(unittest.TestCase):
     @classmethod
@@ -27,6 +55,7 @@ class TestWhiteLists(unittest.TestCase):
         cls.proxy = Web3(Web3.HTTPProvider(proxy_url))
         cls.eth_account = cls.proxy.eth.account.create('https://github.com/neonlabsorg/proxy-model.py/issues/468')
         cls.proxy.eth.default_account = cls.eth_account.address
+        cls.deploy_storage_147_solidity_contract(cls)
 
         cls.client_allowance_token = None
         if NEON_CLIENT_ALLOWANCE_TOKEN is not None:
@@ -60,6 +89,38 @@ class TestWhiteLists(unittest.TestCase):
                                                  TOKEN_PROGRAM_ID,
                                                  cls.signer)
 
+
+    def deploy_storage_147_solidity_contract(self):
+        compiled_sol = compile_source(STORAGE_SOLIDITY_SOURCE_147)
+        contract_id, contract_interface = compiled_sol.popitem()
+        storage = self.proxy.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
+        trx_deploy = self.proxy.eth.account.sign_transaction(dict(
+            nonce=self.proxy.eth.get_transaction_count(self.proxy.eth.default_account),
+            chainId=self.proxy.eth.chain_id,
+            gas=987654321,
+            gasPrice=0,
+            to='',
+            value=0,
+            data=storage.bytecode),
+            self.eth_account.key
+        )
+        print('trx_deploy:', trx_deploy)
+        trx_deploy_hash = self.proxy.eth.send_raw_transaction(trx_deploy.rawTransaction)
+        print('trx_deploy_hash:', trx_deploy_hash.hex())
+        trx_deploy_receipt = self.proxy.eth.wait_for_transaction_receipt(trx_deploy_hash)
+        print('trx_deploy_receipt:', trx_deploy_receipt)
+
+        self.deploy_block_hash = trx_deploy_receipt['blockHash']
+        self.deploy_block_num = trx_deploy_receipt['blockNumber']
+        print('deploy_block_hash:', self.deploy_block_hash)
+        print('deploy_block_num:', self.deploy_block_num)
+
+        self.storage_contract = self.proxy.eth.contract(
+            address=trx_deploy_receipt.contractAddress,
+            abi=storage.abi
+        )                                
+    
+
     def mint_client_allowance_token(self, target: PublicKey, amount: int):
         self.client_allowance_token.mint_to(target, self.mint_authority, amount,
                                             opts=TxOpts(skip_preflight=True, skip_confirmation=False))
@@ -81,6 +142,8 @@ class TestWhiteLists(unittest.TestCase):
         Should reject transaction from sender that was banned
         https://github.com/neonlabsorg/proxy-model.py/issues/468
         """
+
+        print("1st: try to send transaction without banning sender - transaction should complete")
         right_nonce = self.proxy.eth.get_transaction_count(self.proxy.eth.default_account)
         value_to_store = 452356
         trx_store = self.storage_contract.functions.store(value_to_store).buildTransaction({'nonce': right_nonce})
