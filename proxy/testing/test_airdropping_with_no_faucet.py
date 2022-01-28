156f4705d50faa7955dea814d2518e544b38f1e6
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-01-28 17:45:20 +0300

    request_airdrop (320-change-new-user-airdrop)

--- proxy/testing/test_airdropping_with_no_faucet.py
+++ proxy/testing/test_airdropping_with_no_faucet.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import requests
 
 import eth_account
 import eth_typing
@@ -11,11 +12,23 @@ from solana.rpc.api import Client as SolanaClient
 from ..plugin.solana_rest_api_tools import get_token_balance_or_zero, ether2program
 from .testing_helpers import SolidityContractDeployer
 
+def request_airdrop(address):
+    print('==== request_airdrop', address)
+    url = os.environ['FAUCET_URL'] + '/request_neon'
+    data = '{"wallet": "' + address + '", "amount": 100}'
+    r = requests.post(url, data=data)
+    print('==== result', r)
+    if not r.ok:
+        print('Response:', r.status_code)
+    assert(r.ok)
 
 class TestAirdroppingEthAccounts(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        airdrop_amount = 100
+        cls._EXPECTED_BALANCE_WEI = eth_utils.to_wei(airdrop_amount, 'ether')
+
         cls._contract_deployer = SolidityContractDeployer()
         cls._web3 = cls._contract_deployer.web3
 
@@ -24,24 +37,28 @@ class TestAirdroppingEthAccounts(unittest.TestCase):
 
     def test_airdrop_on_get_balance(self):
         account: LocalAccount = eth_account.account.Account.create()
+        request_airdrop(account.address)
         block_number: eth_typing.BlockNumber = self._web3.eth.get_block_number()
         actual_balance_wei = self._web3.eth.get_balance(account.address, block_identifier=block_number)
-        #self.assertEqual(self._EXPECTED_BALANCE_WEI, actual_balance_wei)
+        self.assertEqual(self._EXPECTED_BALANCE_WEI, actual_balance_wei)
 
     def test_airdrop_on_deploy(self):
         contract_owner: LocalAccount = self._web3.eth.account.create()
         contract = self._contract_deployer.compile_and_deploy_contract(contract_owner, self._CONTRACT_STORAGE_SOURCE)
+        request_airdrop(contract.address)
         actual_balance_wei = self._get_balance_wei(contract.address)
-        #self.assertEqual(self._EXPECTED_BALANCE_WEI, actual_balance_wei)
+        self.assertEqual(self._EXPECTED_BALANCE_WEI, actual_balance_wei)
 
     def test_airdrop_onto_wrapped_new_address(self):
         contract_owner: LocalAccount = self._web3.eth.account.create()
         contract = self._contract_deployer.compile_and_deploy_contract(contract_owner, self._WRAPPER_CONTRACT_STORAGE_SOURCE)
         nested_contract_address = contract.functions.getNested().call()
+        request_airdrop(nested_contract_address)
+        request_airdrop(contract.address)
         nested_actual_balance = self._get_balance_wei(nested_contract_address)
         wrapper_actual_balance = self._get_balance_wei(contract.address)
-        #self.assertEqual(self._EXPECTED_BALANCE_WEI, wrapper_actual_balance)
-        #self.assertEqual(self._EXPECTED_BALANCE_WEI, nested_actual_balance)
+        self.assertEqual(self._EXPECTED_BALANCE_WEI, wrapper_actual_balance)
+        self.assertEqual(self._EXPECTED_BALANCE_WEI, nested_actual_balance)
 
     def test_airdrop_on_deploy_estimation(self):
         owner_eth_account: LocalAccount = self._web3.eth.account.create()
@@ -49,8 +66,9 @@ class TestAirdroppingEthAccounts(unittest.TestCase):
         contract_data = compiled_info.contract_interface.get("bin")
         self.assertIsNotNone(contract_data)
         self._web3.eth.estimate_gas({"from": owner_eth_account.address, "data": contract_data})
+        request_airdrop(owner_eth_account.address)
         owner_balance = self._get_balance_wei(owner_eth_account.address)
-        #self.assertEqual(self._EXPECTED_BALANCE_WEI, owner_balance)
+        self.assertEqual(self._EXPECTED_BALANCE_WEI, owner_balance)
 
     def _get_balance_wei(self, eth_account: str) -> int:
         balance = get_token_balance_or_zero(self._solana_client, eth_account)
