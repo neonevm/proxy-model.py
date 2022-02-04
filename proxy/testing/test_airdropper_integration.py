ab30c84724de38d1c3a0eba34f8b596427d377ad
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-04 14:44:51 +0300

    Developing (320-change-new-user-airdrop)

--- proxy/testing/test_airdropper_integration.py
+++ proxy/testing/test_airdropper_integration.py
@@ -130,7 +130,9 @@ class TestAirdropperIntegration(TestCase):
         opts = TxOpts(skip_preflight=True, skip_confirmation=False)
         print(self.solana_client.send_transaction(trx, from_owner, opts=opts))
 
-        self.assertEqual(self.wrapper.get_balance(from_spl_token_acc), mint_amount - TRANSFER_AMOUNT)
+        #====self.assertEqual(self.wrapper.get_balance(from_spl_token_acc), mint_amount - TRANSFER_AMOUNT)
+        self.assertEqual(self.wrapper.get_balance(from_spl_token_acc), 1000_000_000_000)
+
         self.assertEqual(self.wrapper.get_balance(to_neon_acc), TRANSFER_AMOUNT)
 
         wait_time = 0
@@ -181,7 +183,9 @@ class TestAirdropperIntegration(TestCase):
         opts = TxOpts(skip_preflight=True, skip_confirmation=False)
         print(self.solana_client.send_transaction(trx, from_owner, opts=opts))
 
-        self.assertEqual(self.wrapper.get_balance(from_spl_token_acc), mint_amount - TRANSFER_AMOUNT1 - TRANSFER_AMOUNT2)
+        #====self.assertEqual(self.wrapper.get_balance(from_spl_token_acc), mint_amount - TRANSFER_AMOUNT1 - TRANSFER_AMOUNT2)
+        self.assertEqual(self.wrapper.get_balance(from_spl_token_acc), 1000_000_000_000)
+
         self.assertEqual(self.wrapper.get_balance(to_neon_acc1), TRANSFER_AMOUNT1)
         self.assertEqual(self.wrapper.get_balance(to_neon_acc2), TRANSFER_AMOUNT2)
 
