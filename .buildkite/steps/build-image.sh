27acc53d3e37a5c784683addba7abf344706ad90
sinev-valentine <sinev-valentine@yandex.ru>
2022-02-10 16:21:21 +0300

    set evm_loader v.

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 set ${SOLANA_REVISION:=v1.8.12-testnet}
-set ${EVM_LOADER_REVISION:=780bfe46b14d9b288e37906a6c49b3fbe3172d61}
+set ${EVM_LOADER_REVISION:=fb03bdb9abff0617dcbdc93a28a1e03eeb536ca0}
 
 # Refreshing neonlabsorg/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull neonlabsorg/solana:${SOLANA_REVISION}
