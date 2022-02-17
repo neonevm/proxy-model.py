189fa9031573c6266b20522d22d1c145c8263c52
Vasiliy Zaznobin <vz@neonlabs.org>
2022-02-17 11:19:57 +0300

    Remove ci_checks.sh and move the hash checking to build-image.sh

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -euox pipefail
 
 REVISION=$(git rev-parse HEAD)
 
@@ -11,6 +11,25 @@ docker pull neonlabsorg/solana:${SOLANA_REVISION}
 
 # Refreshing neonlabsorg/evm_loader:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull neonlabsorg/evm_loader:${EVM_LOADER_REVISION}
+
+ls -al
+
+INFRA_REFLECT_FILE="proxy-model.py.changes"
+INFRA_REFLECT_REPO_PATH="https://raw.githubusercontent.com/neonlabsorg/proxy-model.py/369-calculate-hashes/develop_changes/"
+MAINTENANCE_FILES="
+./proxy/environment.py
+./proxy/proxy.py"
+
+echo "MAINTENANCE_FILES=$MAINTENANCE_FILES"
+wget -O "${INFRA_REFLECT_FILE}" "${INFRA_REFLECT_REPO_PATH}${INFRA_REFLECT_FILE}"
+git ls-files -s $MAINTENANCE_FILES > "${INFRA_REFLECT_FILE}"".""${REVISION}"
+cat ./"${INFRA_REFLECT_FILE}"
+cat ./"${INFRA_REFLECT_FILE}"".""${REVISION}"
+if diff -B ./"${INFRA_REFLECT_FILE}" ./"${INFRA_REFLECT_FILE}"".""${REVISION}"; then
+  echo "The changes in maintenance files: "$MAINTENANCE_FILES "are reflected in the infra file ${INFRA_REFLECT_REPO_PATH}${INFRA_REFLECT_FILE}";
+else
+  echo "The changes in maintenance files: "$MAINTENANCE_FILES "are NOT reflected in the infra file ${INFRA_REFLECT_REPO_PATH}${INFRA_REFLECT_FILE}";
+fi
 
 docker build -t neonlabsorg/proxy:${REVISION} \
     --build-arg SOLANA_REVISION=${SOLANA_REVISION} \
