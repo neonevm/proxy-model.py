768bf095c91871c0d0232c5023eb65cd18bd6bf5
Vasiliy Zaznobin <vz@neonlabs.org>
2022-02-21 10:39:14 +0300

    Introduce run-db-creation.sh

---
+++ proxy/db_creation.py
@@ -0,0 +1,19 @@
+import sys
+
+from proxy.common_neon.costs import SQLCost
+from proxy.indexer.accounts_db import NeonAccountDB
+from proxy.indexer.airdropper import AirdropReadySet, FailedAttempts
+from proxy.indexer.blocks_db import SolanaBlocksDB
+from proxy.indexer.logs_db import LogsDB
+from proxy.indexer.transactions_db import NeonTxsDB
+from proxy.indexer.trx_receipts_storage import TrxReceiptsStorage
+
+db_operator_cost = SQLCost()
+db_solana_blocks = SolanaBlocksDB()
+db_neon_txs = NeonTxsDB()
+db_neon_account = NeonAccountDB()
+db_airdrop_ready = AirdropReadySet()
+db_failed_attempts = FailedAttempts()
+db_indexer_logs = LogsDB()
+db_transaction_receipts = TrxReceiptsStorage('transaction_receipts')
+
---
+++ proxy/run-db-creation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+COMPONENT="DB-Creation"
+echo "$(date "+%F %X.%3N") I $(basename $0):${LINENO} $$ ${COMPONENT}:StartScript {} Start ${COMPONENT} service"
+
+source proxy/run-set-env.sh ${COMPONENT}
+
+echo "$(date "+%F %X.%3N") I $(basename $0):${LINENO} $$ ${COMPONENT}:StartScript {} db-creation"
+python3 db_creation.py
