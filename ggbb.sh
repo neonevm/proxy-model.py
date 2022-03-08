2f1531e28b1a3174977a7f72df7db1d9d3e9c427
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-08 17:52:03 +0500

    Enable full test suite )

---
+++ ggbb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+cat << EOF
+24 passing (2m)
+
+`stdout` files found: 83. Processing ...
+
+Summarize result:
+
+    Failing - 33
+    Pending - 121
+    Passing - 934
+
+Total tests - 1088
+
+Test files without test result - 0:
+
+Fix allure results: 0
+The allure-results archive is creating at: /opt/allure-reports.tar
+
+EOF
