cc853d17a2c48ebc1164e906182b755beddfb5c3
Andrey Voloshin <andrey@scalr.com>
2022-03-15 09:00:49 +0300

    wip

--- tf/destroy.sh
+++ tf/destroy.sh
@@ -9,6 +9,7 @@ mkdir -p $ARTIFACTS_LOGS
 
 # solana
 export REMOTE_HOST=`buildkite-agent meta-data get "SOLANA_IP"`
+ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
 ssh -i $SSH_KEY ubuntu@$REMOTE_HOST 'sudo docker logs solana > /tmp/solana.log'
 scp -i $SSH_KEY ubuntu@$REMOTE_HOST:/tmp/solana.log $ARTIFACTS_LOGS
 
