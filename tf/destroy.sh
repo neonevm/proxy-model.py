fd0cb9959034773351ab5f4f7d0ec17571f61768
Andrey Voloshin <andrey@scalr.com>
2022-03-16 18:50:17 +0300

    wip

--- tf/destroy.sh
+++ tf/destroy.sh
@@ -35,7 +35,7 @@ export TFSTATE_REGION="us-east-2"
 export TF_VAR_neon_evm_revision=latest
 export TF_VAR_proxy_model_revision=latest
 export TF_BACKEND_CONFIG="-backend-config="bucket=${TFSTATE_BUCKET}" -backend-config="key=${TFSTATE_KEY}" -backend-config="region=${TFSTATE_REGION}""
-#terraform init $TF_BACKEND_CONFIG
+terraform init $TF_BACKEND_CONFIG
 #terraform destroy --auto-approve=true
 
 
