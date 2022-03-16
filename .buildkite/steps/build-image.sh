522baa3119e99c5a8e1ce04f34788803a552c9ce
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-03-16 15:08:02 +0400

    Last changes from v0.7.x to develop

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 set ${SOLANA_REVISION:=v1.8.12-testnet}
-set ${EVM_LOADER_REVISION:=v0.7.1}
+set ${EVM_LOADER_REVISION:=latest}
 
 # Refreshing neonlabsorg/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull neonlabsorg/solana:${SOLANA_REVISION}
