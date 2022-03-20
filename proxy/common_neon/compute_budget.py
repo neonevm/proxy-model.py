660adf8ac835e9cda771cee88fd0822a23c78108
Andrey Trepalin <at@neonlabs.org>
2022-03-20 15:59:09 +0000

    #666 Updated to new compute budget requests with additional_fee

--- proxy/common_neon/compute_budget.py
+++ proxy/common_neon/compute_budget.py
@@ -3,11 +3,11 @@ from .constants import COMPUTE_BUDGET_ID
 
 class ComputeBudget():
     @staticmethod
-    def requestUnits(units):
+    def requestUnits(units, additional_fee):
         return TransactionInstruction(
             program_id=COMPUTE_BUDGET_ID,
             keys=[],
-            data=bytes.fromhex("00")+units.to_bytes(4,"little")
+            data=bytes.fromhex("00")+units.to_bytes(4,"little")+additional_fee.to_bytes(4,"little")
         )
 
     @staticmethod
@@ -20,9 +20,10 @@ class ComputeBudget():
 
 DEFAULT_UNITS=500*1000
 DEFAULT_HEAP_FRAME=256*1024
+DEFAULT_ADDITIONAL_FEE=0
 
-def TransactionWithComputeBudget(units=DEFAULT_UNITS, heapFrame=DEFAULT_HEAP_FRAME, **args):
+def TransactionWithComputeBudget(units=DEFAULT_UNITS, additional_fee=DEFAULT_ADDITIONAL_FEE, heapFrame=DEFAULT_HEAP_FRAME, **args):
     trx = Transaction(**args)
-    if units: trx.add(ComputeBudget.requestUnits(units))
+    if units: trx.add(ComputeBudget.requestUnits(units, additional_fee))
     if heapFrame: trx.add(ComputeBudget.requestHeapFrame(heapFrame))
     return trx
