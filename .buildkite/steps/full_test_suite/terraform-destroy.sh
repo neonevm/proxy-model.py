5d4a10ca79c43e858a6e0afe8046c9d26b76fc41
Andrey Voloshin <andrey@scalr.com>
2022-03-22 17:49:18 +0300

    wip

--- .buildkite/steps/full_test_suite/terraform-destroy.sh
+++ .buildkite/steps/full_test_suite/terraform-destroy.sh
@@ -12,8 +12,8 @@ mkdir -p $ARTIFACTS_LOGS
 # solana
 export REMOTE_HOST=`buildkite-agent meta-data get "SOLANA_IP"`
 ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
-ssh -i $SSH_KEY ubuntu@$REMOTE_HOST 'sudo docker logs solana > /tmp/solana.log 2>&1'
-scp -i $SSH_KEY ubuntu@$REMOTE_HOST:/tmp/solana.log $ARTIFACTS_LOGS
+ssh -i ${SSH_KEY} ubuntu@${REMOTE_HOST} 'sudo docker logs solana > /tmp/solana.log 2>&1'
+scp -i ${SSH_KEY} ubuntu@${REMOTE_HOST}:/tmp/solana.log ${ARTIFACTS_LOGS}
 
 
 # proxy
@@ -24,13 +24,13 @@ declare -a services=("evm_loader" "postgres" "dbcreation" "indexer" "proxy" "fau
 for service in "${services[@]}"
 do
    echo "$servce"
-   ssh -i $SSH_KEY ubuntu@$REMOTE_HOST "sudo docker logs $service > /tmp/$service.log 2>&1"
-   scp -i $SSH_KEY ubuntu@$REMOTE_HOST:/tmp/$service.log $ARTIFACTS_LOGS
+   ssh -i ${SSH_KEY} ubuntu@${REMOTE_HOST} "sudo docker logs $service > /tmp/$service.log 2>&1"
+   scp -i ${SSH_KEY} ubuntu@${REMOTE_HOST}:/tmp/$service.log ${ARTIFACTS_LOGS}
 done
 
 
 ### Clean infrastructure by terraform
-export TF_VAR_branch=$BUILDKITE_BRANCH
+export TF_VAR_branch=${BUILDKITE_BRANCH}
 export TFSTATE_BUCKET="nl-ci-stands"
 export TFSTATE_KEY="tests/test-$BUILDKITE_COMMIT"
 export TFSTATE_REGION="us-east-2"
