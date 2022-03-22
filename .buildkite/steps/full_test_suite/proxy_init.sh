6a96892ba9af366291a05c23ae127c74b092ecdc
Andrey Voloshin <andrey@scalr.com>
2022-03-22 17:18:35 +0300

    Revert "wip"
    This reverts commit f8e17fb058435fdfe0c894cb6ee0931b02bd8310.

--- .buildkite/steps/full_test_suite/proxy_init.sh
+++ .buildkite/steps/full_test_suite/proxy_init.sh
@@ -23,7 +23,7 @@ curl -O https://raw.githubusercontent.com/neonlabsorg/proxy-model.py/${branch}/p
 
 # Set required environment variables
 export REVISION=${revision}
-export SOLANA_URL=http:\/\/$solana_ip:8899
+export SOLANA_URL=http:\/\/${solana_ip}:8899
 
 
 # Generate docker-compose override file
@@ -34,13 +34,13 @@ services:
   evm_loader:
     container_name: evm_loader
     environment:
-      - SOLANA_URL=$SOLANA_URL
+      - SOLANA_URL=${SOLANA_URL}
     networks:
       - net
     command: bash -c "create-test-accounts.sh 1 && deploy-evm.sh && /opt/spl-token create-account HPsV9Deocecw3GeZv1FkAPNCBRfuVyfw9MMwjwRe1xaU && /opt/spl-token mint HPsV9Deocecw3GeZv1FkAPNCBRfuVyfw9MMwjwRe1xaU 1000000000 --owner /opt/evm_loader-keypair.json -- HX14J4Pp9CgSbWP13Dtpm8VLJpNxMYffLtRCRGsx7Edv"
   proxy:
     environment:
-      - SOLANA_URL=$SOLANA_URL
+      - SOLANA_URL=${SOLANA_URL}
     ports:
       - 9091:9090
   faucet:
@@ -48,7 +48,7 @@ services:
     environment:
       - FAUCET_RPC_BIND=0.0.0.0
       - FAUCET_RPC_PORT=3333
-      - SOLANA_URL=$SOLANA_URL
+      - SOLANA_URL=${SOLANA_URL}
       - NEON_ETH_MAX_AMOUNT=50000
       - EVM_LOADER=53DfF883gyixYNXnM7s5xhdeyV8mVk9T4i2hGV9vG9io
       - NEON_TOKEN_MINT=HPsV9Deocecw3GeZv1FkAPNCBRfuVyfw9MMwjwRe1xaU   
@@ -63,10 +63,10 @@ services:
     entrypoint: /opt/faucet/faucet --config /opt/proxy/faucet.conf run
   airdropper:
     environment:
-      - SOLANA_URL=$SOLANA_URL
+      - SOLANA_URL=${SOLANA_URL}
   indexer:
     environment:
-      - SOLANA_URL=$SOLANA_URL
+      - SOLANA_URL=${SOLANA_URL}
 EOF
 
 
@@ -75,10 +75,10 @@ SERVICES=$(docker-compose -f docker-compose-test.yml config --services | grep -v
 
 
 # Check if Solana is available, max attepts is 100(each for 2 seconds)
-CHECK_COMMAND=`curl $SOLANA_URL -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getHealth"}'`
+CHECK_COMMAND=`curl ${SOLANA_URL} -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getHealth"}'`
 MAX_COUNT=100
 CURRENT_ATTEMPT=1
-while [[ "$CHECK_COMMAND" != "{\"jsonrpc\":\"2.0\",\"result\":\"ok\",\"id\":1}" && $CURRENT_ATTEMPT -gt $MAX_COUNT ]]
+while [[ "$CHECK_COMMAND" != "{\"jsonrpc\":\"2.0\",\"result\":\"ok\",\"id\":1}" && ${CURRENT_ATTEMPT} -gt ${MAX_COUNT} ]]
 do
   CHECK_COMMAND=`curl $SOLANA_URL -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getHealth"}'`
   ((CURRENT_ATTEMPT=CURRENT_ATTEMPT+1))
--- .buildkite/steps/full_test_suite/terraform-build.sh
+++ .buildkite/steps/full_test_suite/terraform-build.sh
@@ -9,13 +9,18 @@ export TF_VAR_branch=${BUILDKITE_BRANCH}
 export TFSTATE_BUCKET="nl-ci-stands"
 export TFSTATE_KEY="tests/test-$BUILDKITE_COMMIT"
 export TFSTATE_REGION="us-east-2"
-export TF_VAR_neon_evm_revision=${NEON_EVM_COMMIT:-latest}
-export TF_VAR_proxy_model_revision=${BUILDKITE_COMMIT:-latest}
+export TF_VAR_neon_evm_revision=latest
+export TF_VAR_proxy_model_revision=latest
 export TF_BACKEND_CONFIG="-backend-config="bucket=${TFSTATE_BUCKET}" -backend-config="key=${TFSTATE_KEY}" -backend-config="region=${TFSTATE_REGION}""
 terraform init ${TF_BACKEND_CONFIG}
 terraform apply --auto-approve=true
 
 
-# Save IPs for next steps
+# Get IPs
 terraform output --json | jq -r '.proxy_ip.value' | buildkite-agent meta-data set "PROXY_IP"
 terraform output --json | jq -r '.solana_ip.value' | buildkite-agent meta-data set "SOLANA_IP"
+
+
+# Save IPs for next steps
+buildkite-agent meta-data get "PROXY_IP"
+buildkite-agent meta-data get "SOLANA_IP"
