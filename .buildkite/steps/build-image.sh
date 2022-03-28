b276100ca13160a9788fb24b050938e7936f90e8
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-03-28 06:58:15 +0300

    Latest changes from develop (#679)
    * Move CI operator keypairs to safety location
    * Add key storage for private accounts. #651
    * Add eth_accounts. #647
    * Add eth_sign. #651
    * Add eth_signTransaction. #652
    * Add eth_sendTransaction. #654
    * Add web_sha3. #658
    * Add eth_getTransactionByBlockNumberAndIndex. #656
    * Add eth_getTransactionByBlockHashAndIndex. #655
    * Add eth_getBlockTransactionCountByHash. #646
    * Change message about bad tx nonce in precheck. #609
    * Add eth_mining. #650
    * Add environment variable to enable PRIVATE API.
    * Add parsing of nonce error from Solana receipt. #609
    * Readable error about influence balance. #609
    * Add error about out of gas. #609
    * Add test for transfer of not enough funds. #609
    * Add checking return code of neon-cli emulator. #678
    * Validation of neon tx before exectution. #602
    
    Co-authored-by: Andrey Trepalin <ai.trepalin@gmail.com>
    Co-authored-by: Andrey Trepalin <at@neonlabs.org>
    Co-authored-by: Denis K <100715772+dk-neon@users.noreply.github.com>
    Co-authored-by: Andrey Voloshin <inkognitotester@gmail.com>
    Co-authored-by: rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
    Co-authored-by: Andrey Voloshin <andrey@scalr.com>
    Co-authored-by: Andrey Voloshin <vo@neonlabs.org>

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,17 +3,17 @@ set -euo pipefail
 
 REVISION=$(git rev-parse HEAD)
 
-set ${SOLANA_REVISION:=v1.8.12-testnet}
-set ${EVM_LOADER_REVISION:=v0.7.1}
+set ${SOLANA_REVISION:=v1.9.12-testnet}
+set ${NEON_EVM_COMMIT:=latest}
 
 # Refreshing neonlabsorg/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull neonlabsorg/solana:${SOLANA_REVISION}
 
 # Refreshing neonlabsorg/evm_loader:latest image is required to run .buildkite/steps/build-image.sh locally
-docker pull neonlabsorg/evm_loader:${EVM_LOADER_REVISION}
+docker pull neonlabsorg/evm_loader:${NEON_EVM_COMMIT}
 
 docker build -t neonlabsorg/proxy:${REVISION} \
     --build-arg SOLANA_REVISION=${SOLANA_REVISION} \
-    --build-arg EVM_LOADER_REVISION=${EVM_LOADER_REVISION} \
+    --build-arg NEON_EVM_COMMIT=${NEON_EVM_COMMIT} \
     --build-arg PROXY_REVISION=${REVISION} \
     .
