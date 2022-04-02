b265ada9acf8d972375a13cebbf62630f7d931c9
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-04-02 11:19:56 +0300

    Sort solana transactions by exection time.
    * Sort solana transactions by exection time in indexer.
    * Remove depends on Ethereum transaction in Canceler.
    * Add transaction canceling on selecting operator resource.
    * Fix eth_syncing.
    * Add transaction index.
    * Fix bug with already processed solana transaction.
    * Add predictable block hashes for empty block slots.

--- proxy/common_neon/transaction_validator.py
+++ proxy/common_neon/transaction_validator.py
@@ -24,7 +24,7 @@ class NeonTxValidator:
         self._tx = tx
 
         self._sender = '0x' + tx.sender()
-        self._account_info = self._solana.get_account_info_layout(EthereumAddress(self._sender))
+        self._neon_account_info = self._solana.get_neon_account_info(EthereumAddress(self._sender))
 
         self._deployed_contract = tx.contract()
         if self._deployed_contract:
@@ -77,26 +77,26 @@ class NeonTxValidator:
             raise EthereumError(message='transaction size is too big')
 
     def _prevalidate_tx_nonce(self):
-        if not self._account_info:
+        if not self._neon_account_info:
             return
 
         tx_nonce = int(self._tx.nonce)
-        if self.MAX_U64 not in (self._account_info.trx_count, tx_nonce):
-            if tx_nonce == self._account_info.trx_count:
+        if self.MAX_U64 not in (self._neon_account_info.trx_count, tx_nonce):
+            if tx_nonce == self._neon_account_info.trx_count:
                 return
 
-        self._raise_nonce_error(self._account_info.trx_count, tx_nonce)
+        self._raise_nonce_error(self._neon_account_info.trx_count, tx_nonce)
 
     def _prevalidate_sender_eoa(self):
-        if not self._account_info:
+        if not self._neon_account_info:
             return
 
-        if self._account_info.code_account:
+        if self._neon_account_info.code_account:
             raise EthereumError("sender not an eoa")
 
     def _prevalidate_sender_balance(self):
-        if self._account_info:
-            user_balance = self._account_info.balance
+        if self._neon_account_info:
+            user_balance = self._neon_account_info.balance
         else:
             user_balance = 0
 
