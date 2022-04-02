e7dba6024ae951084b831edb2692fea0449a5023
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-04-02 23:02:14 +0400

    Get rid of extra logging

--- .buildkite/steps/full_test_suite/terraform-destroy.sh
+++ .buildkite/steps/full_test_suite/terraform-destroy.sh
@@ -13,7 +13,7 @@ mkdir -p $ARTIFACTS_LOGS
 # solana
 export REMOTE_HOST=`buildkite-agent meta-data get "SOLANA_IP"`
 ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
-ssh -i ${SSH_KEY} ubuntu@${REMOTE_HOST} 'sudo docker logs solana | pbzip2 > /tmp/solana.log.bz2'
+ssh -i ${SSH_KEY} ubuntu@${REMOTE_HOST} 'sudo docker logs solana 2>&1 | pbzip2 > /tmp/solana.log.bz2'
 scp -i ${SSH_KEY} ubuntu@${REMOTE_HOST}:/tmp/solana.log.bz2 ${ARTIFACTS_LOGS}
 
 
@@ -25,7 +25,7 @@ declare -a services=("evm_loader" "postgres" "dbcreation" "indexer" "proxy" "fau
 for service in "${services[@]}"
 do
    echo "$service"
-   ssh -i ${SSH_KEY} ubuntu@${REMOTE_HOST} "sudo docker logs $service | pbzip2 > /tmp/$service.log.bz2"
+   ssh -i ${SSH_KEY} ubuntu@${REMOTE_HOST} "sudo docker logs $service 2>&1 | pbzip2 > /tmp/$service.log.bz2"
    scp -i ${SSH_KEY} ubuntu@${REMOTE_HOST}:/tmp/$service.log.bz2 ${ARTIFACTS_LOGS}
 done
 
