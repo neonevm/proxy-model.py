a43b370a7bbfa3c8939444c04efeeea146652e6c
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-04-03 11:58:58 +0400

    Move pbzip2 into different command

--- .buildkite/steps/full_test_suite/proxy_init.sh
+++ .buildkite/steps/full_test_suite/proxy_init.sh
@@ -4,12 +4,13 @@
 # Install docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get -y install ca-certificates curl gnupg lsb-release pbzip2
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
+sudo apt-get -y install pbzip2
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
