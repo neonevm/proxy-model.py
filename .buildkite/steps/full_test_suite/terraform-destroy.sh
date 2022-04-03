4b35a2bb76153fa294730955c40ea091795aad0f
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-04-03 13:54:18 +0400

    Revert "move logs into fts step"
    This reverts commit 26e6a39052db7132b7af106284d295e6c3bcd090.

--- .buildkite/steps/full_test_suite/terraform-destroy.sh
+++ .buildkite/steps/full_test_suite/terraform-destroy.sh
@@ -6,6 +6,29 @@ cd .buildkite/steps/full_test_suite
 
 ### Receive artefacts
 export SSH_KEY="~/.ssh/ci-stands"
+export ARTIFACTS_LOGS="./logs"
+mkdir -p $ARTIFACTS_LOGS
+
+
+# solana
+export SOLANA_HOST=`buildkite-agent meta-data get "SOLANA_IP"`
+ssh-keyscan -H $SOLANA_HOST >> ~/.ssh/known_hosts
+echo "Upload logs for service: solana"
+ssh -i ${SSH_KEY} ubuntu@${SOLANA_HOST} 'sudo docker logs solana 2>&1 | pbzip2 > /tmp/solana.log.bz2'
+scp -i ${SSH_KEY} ubuntu@${SOLANA_HOST}:/tmp/solana.log.bz2 ${ARTIFACTS_LOGS}
+
+
+# proxy
+export PROXY_HOST=`buildkite-agent meta-data get "PROXY_IP"`
+ssh-keyscan -H $PROXY_HOST >> ~/.ssh/known_hosts
+declare -a services=("evm_loader" "postgres" "dbcreation" "indexer" "proxy" "faucet" "airdropper")
+
+for service in "${services[@]}"
+do
+   echo "Upload logs for service: $service"
+   ssh -i ${SSH_KEY} ubuntu@${PROXY_HOST} "sudo docker logs $service 2>&1 | pbzip2 > /tmp/$service.log.bz2"
+   scp -i ${SSH_KEY} ubuntu@${PROXY_HOST}:/tmp/$service.log.bz2 ${ARTIFACTS_LOGS}
+done
 
 export NEON_EVM_COMMIT=${NEON_EVM_COMMIT:-latest}
 export PROXY_MODEL_COMMIT=${BUILDKITE_COMMIT}
