ee66beb5f0011ec8c5f5d1648a82c8aa615ed61e
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-04-07 00:06:06 +0300

    Set version to 0.7.13-dev

--- proxy/plugin/solana_rest_api.py
+++ proxy/plugin/solana_rest_api.py
@@ -50,7 +50,7 @@ from ..statistics_exporter.prometheus_proxy_exporter import PrometheusExporter
 modelInstanceLock = threading.Lock()
 modelInstance = None
 
-NEON_PROXY_PKG_VERSION = '0.7.11-dev'
+NEON_PROXY_PKG_VERSION = '0.7.13-dev'
 NEON_PROXY_REVISION = 'NEON_PROXY_REVISION_TO_BE_REPLACED'
 
 
