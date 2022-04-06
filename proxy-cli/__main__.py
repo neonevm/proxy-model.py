973d68d34ea2d9950ab6465c52d6d9321b139b7b
Dmitriy Borisenko <otselnik@yandex.ru>
2022-04-06 19:30:13 +0300

    Remove operator command

--- proxy-cli/__main__.py
+++ proxy-cli/__main__.py
@@ -2,7 +2,6 @@ import argparse
 import sys
 
 from .account import AccountHandler
-from .operator import OperatorHandler
 from .info import InfoHandler
 
 if __name__ == '__main__':
@@ -10,14 +9,11 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers(title='command', dest='command', description='valid commands')
 
     account_handler = AccountHandler.init_args_parser(subparsers)
-    operator_handler = OperatorHandler.init_args_parser(subparsers)
     info_handler = InfoHandler.init_args_parser(subparsers)
 
     args = parser.parse_args()
     if args.command == account_handler.command:
         account_handler.execute(args)
-    elif args.command == operator_handler.command:
-        operator_handler.execute(args)
     elif args.command == info_handler.command:
         info_handler.execute(args)
     else:
