114811fb16f59d93a24d9d78440d83bd63bb275b
Dmitriy Borisenko <otselnik@yandex.ru>
2022-04-07 11:24:04 +0300

    print balance in sols or neons

--- proxy-cli/info.py
+++ proxy-cli/info.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from argparse import _SubParsersAction, ArgumentParser
+from decimal import Decimal
 
 import sys
 import math
@@ -15,6 +16,13 @@ neon_logger.setLevel(logging.CRITICAL)
 from proxy.common_neon.address import EthereumAddress, accountWithSeed
 from proxy.common_neon.solana_interactor import SolanaInteractor
 from proxy.environment import PERM_ACCOUNT_LIMIT, SOLANA_URL, get_solana_accounts
+
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
 
 
 class InfoHandler:
@@ -53,7 +61,7 @@ class InfoHandler:
         elif args.subcommand == None:
             self.print_stdout = False
             ret_js = self._all_info(args)
-            print(json.dumps(ret_js))
+            print(json.dumps(ret_js, cls=DecimalEncoder))
         else:
             print(f'Unknown command {args.subcommand} for account', file=sys.stderr)
             return
@@ -127,12 +135,12 @@ class InfoHandler:
             acc_info_js['address'] = str(neon_account)
             acc_info_js['balance'] = self._get_neon_balance(neon_account)
 
-            self._print(f"{acc_info_js['address']}    {acc_info_js['balance']}")
+            self._print(f"{acc_info_js['address']}    {acc_info_js['balance']:,.18f}")
 
             ret_js['total_balance'] += acc_info_js['balance']
             ret_js['neon-accounts'].append(acc_info_js)
 
-        self._print(f"total_balance    {ret_js['total_balance']}")
+        self._print(f"total_balance    {ret_js['total_balance']:,.18f}")
         return ret_js
 
     def _solana_accounts_info(self, args):
@@ -145,22 +153,22 @@ class InfoHandler:
 
         for sol_account in operator_accounts:
             acc_info_js = self._get_solana_accounts(sol_account)
-            self._print(f"{acc_info_js['address']}    {acc_info_js['balance']}")
+            self._print(f"{acc_info_js['address']}    {acc_info_js['balance']:,.9f}")
 
             ret_js['total_balance'] += acc_info_js['balance']
             self._print(f"holder:")
             for holder_account in acc_info_js['holder']:
-                self._print(f"    {acc_info_js['address']}    {acc_info_js['balance']}")
+                self._print(f"    {acc_info_js['address']}    {acc_info_js['balance']:,.9f}")
                 ret_js['resource_balance'] += holder_account['balance']
             self._print(f"storage:")
             for storage_account in acc_info_js['storage']:
-                self._print(f"    {acc_info_js['address']}    {acc_info_js['balance']}")
+                self._print(f"    {acc_info_js['address']}    {acc_info_js['balance']:,.9f}")
                 ret_js['resource_balance'] += storage_account['balance']
 
             ret_js['accounts'].append(acc_info_js)
 
-        self._print(f"total_balance       {ret_js['total_balance']}")
-        self._print(f"resource_balance    {ret_js['resource_balance']}")
+        self._print(f"total_balance       {ret_js['total_balance']:,.9f}")
+        self._print(f"resource_balance    {ret_js['resource_balance']:,.9f}")
         return ret_js
 
     def _all_info(self, args):
@@ -210,7 +218,7 @@ class InfoHandler:
 
     def _get_neon_balance(self, neon_address: EthereumAddress):
         neon_layout = self._solana.get_neon_account_info(neon_address)
-        return neon_layout.balance if neon_layout else 0
+        return Decimal(neon_layout.balance) / 1_000_000_000 / 1_000_000_000 if neon_layout else 0
 
     def _get_solana_accounts(self, sol_account):
         resource_tags = {
@@ -226,8 +234,7 @@ class InfoHandler:
 
         acc_info_js = {}
         acc_info_js['address'] = str(sol_account.public_key())
-        acc_info_js['balance'] = self._solana.get_sol_balance(sol_account.public_key())
-        self._print(f"address: {acc_info_js['address']}")
+        acc_info_js['balance'] = Decimal(self._solana.get_sol_balance(sol_account.public_key())) / 1_000_000_000
 
         acc_info_js['storage'] = []
         acc_info_js['holder'] = []
@@ -241,13 +248,13 @@ class InfoHandler:
                 storage_account = {}
                 storage_account['address'] = str(storage_address)
                 storage_account['status'] = resource_tags.get(storage_info.tag, 'UNKNOWN')
-                storage_account['balance'] = storage_info.lamports
+                storage_account['balance'] = Decimal(storage_info.lamports) / 1_000_000_000
                 acc_info_js['storage'].append(storage_account)
             if holder_info:
                 holder_account = {}
                 holder_account['address'] = str(holder_address)
                 holder_account['status'] = resource_tags.get(holder_info.tag, 'UNKNOWN')
-                holder_account['balance'] = holder_info.lamports
+                holder_account['balance'] = Decimal(holder_info.lamports) / 1_000_000_000
                 acc_info_js['holder'].append(holder_account)
 
         return acc_info_js
