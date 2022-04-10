1903f4412c02c2857e3231244e020aa92ba526d8
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-04-10 19:33:57 +0400

    Spit and polish

--- proxy/mempool/mem_pool.py
+++ proxy/mempool/mem_pool.py
@@ -5,19 +5,20 @@ from multiprocessing import Pool
 @logged_group("neon.Proxy")
 class MemPool:
 
-    POOL_PROC_COUNT = 4
+    POOL_PROC_COUNT = 20
 
     def __init__(self):
         self._pool = Pool(processes=self.POOL_PROC_COUNT)
 
     def on_eth_send_raw_transaction(self, *, eth_trx_hash):
-        self._pool.apply_async(self._on_eth_send_raw_transaction_impl, (eth_trx_hash,), {}, self.on_sending_trx_proceed, self.error_callback)
+        self._pool.apply_async(func=self._on_eth_send_raw_transaction_impl, args=(eth_trx_hash,),
+                               callback=self.on_eth_send_raw_transaction_callback, error_callback=self.error_callback)
 
     def error_callback(self, error):
-        self.error("Failed to invoke the function on worker process: ", error)
+        self.error("Failed to invoke on worker process: ", error)
 
-    def on_sending_trx_proceed(self, result):
-        self.debug(f"Sending transaction proceed: {result}")
+    def on_eth_send_raw_transaction_callback(self, result):
+        pass
 
     def _on_eth_send_raw_transaction_impl(self, eth_trx_hash):
         self.debug(f"Transaction is being processed on the worker: {eth_trx_hash}")
--- proxy/mempool/mempool_service.py
+++ proxy/mempool/mempool_service.py
@@ -11,15 +11,16 @@ class MemPoolService(QueueBasedService):
     MEM_POOL_SERVICE_PORT = 9091
 
     def __init__(self):
-        QueueBasedService.__init__(self, self.MEM_POOL_SERVICE_PORT)
-        self._mem_pool = MemPool()
+        QueueBasedService.__init__(self, self.MEM_POOL_SERVICE_PORT, True)
+        self._mem_pool = None
 
     def on_eth_send_raw_transaction(self, *, eth_trx_hash):
         self._mem_pool.on_eth_send_raw_transaction(eth_trx_hash=eth_trx_hash)
 
+    # QueueBasedService abstracts
+
+    def service_process_init(self):
+        self._mem_pool = MemPool()
+
     def do_extras(self):
         self._mem_pool.do_extras()
-
-    def __del__(self):
-        self.info("Delete Mempool service")
-
