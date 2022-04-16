cbfc8bbb9891573c65849eb6c9a13d85a3a3ffbe
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-04-16 18:03:09 +0400

    Simplify start mechanism

--- proxy/neon_proxy_app.py
+++ proxy/neon_proxy_app.py
@@ -7,9 +7,9 @@ from .statistics_exporter.prometheus_proxy_server import PrometheusProxyServer
 class NeonProxyApp:
 
     def __init__(self):
-        self._mempool_service = MemPoolService(is_background=True)
+        self._mempool_service = MemPoolService()
 
     def start(self):
-        PrometheusProxyServer()
         self._mempool_service.start()
+        PrometheusProxyServer()
         entry_point()
