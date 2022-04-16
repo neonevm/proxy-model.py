d956dc7e3537d6075e210f04e7edb3f17e5c238e
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-04-16 18:12:14 +0400

    mempool init

--- proxy/mempool/__init__.py
+++ proxy/mempool/__init__.py
@@ -1,3 +1,8 @@
+from .mempool_server import MemPoolServer
+from .mempool_client import MemPoolClient
 from .mempool_service import MemPoolService
 from .mempool_client import MemPoolClient
 from .mem_pool import MemPool
+
+MEMPOOL_SERVICE_PORT = MemPoolService.MEMPOOL_SERVICE_PORT
+MEMPOOL_SERVICE_HOST = MemPoolService.MEMPOOL_SERVICE_HOST
