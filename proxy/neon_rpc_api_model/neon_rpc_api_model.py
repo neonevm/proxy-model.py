50c3c014027868d9146fbeea35de463440c15a3f
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-05-07 13:34:12 +0530

    feat: add option to return earliest block on 0x0
    The graph's own API doesn't handle it well when the block returned during it's origin search returns an invalid-block (since 0x0 doesn't exist on Neon)
    
    The patch gives the option to return the earliest known block when 0x0 or 0 is passed to the eth_getBlockByNumber RPC
    
    Enable using USE_EARLIEST_BLOCK_IF_0_PASSED env flag

--- proxy/neon_rpc_api_model/neon_rpc_api_model.py
+++ proxy/neon_rpc_api_model/neon_rpc_api_model.py
@@ -17,7 +17,7 @@ from ..common_neon.solana_interactor import SolanaInteractor
 from ..common_neon.utils import SolanaBlockInfo
 from ..common_neon.types import NeonTxPrecheckResult, NeonEmulatingResult
 from ..environment import SOLANA_URL, PP_SOLANA_URL, PYTH_MAPPING_ACCOUNT, NEON_EVM_VERSION, NEON_EVM_REVISION, \
-                          CHAIN_ID, neon_cli, EVM_STEP_COUNT
+                          CHAIN_ID, USE_EARLIEST_BLOCK_IF_0_PASSED, neon_cli, EVM_STEP_COUNT
 from ..memdb.memdb import MemDB
 from ..common_neon.gas_price_calculator import GasPriceCalculator
 from ..statistics_exporter.proxy_metrics_interface import StatisticsExporter
@@ -106,10 +106,13 @@ class NeonRpcApiModel:
     def __repr__(self):
         return str(self.__dict__)
 
+    def _should_return_starting_block(self, tag) -> bool:
+        return tag == 'earliest' \
+            or ((tag == '0x0' or str(tag) == '0') and USE_EARLIEST_BLOCK_IF_0_PASSED)
     def _process_block_tag(self, tag) -> SolanaBlockInfo:
         if tag in ("latest", "pending"):
             block = self._db.get_latest_block()
-        elif tag == 'earliest':
+        elif self._should_return_starting_block(tag):
             block = self._db.get_starting_block()
         elif isinstance(tag, str):
             try:
