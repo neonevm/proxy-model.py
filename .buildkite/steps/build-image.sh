1ce0b0f92330cbe3e4bf949af5adfedc7a0b14e9
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-05-14 11:30:16 +0300

    Update evm version

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REVISION=$(git rev-parse HEAD)
 
 set ${SOLANA_REVISION:=v1.9.12-testnet-with_trx_cap}
-set ${NEON_EVM_COMMIT:=v0.7.5}
+set ${NEON_EVM_COMMIT:=v0.7.7}
 
 # Refreshing neonlabsorg/solana:latest image is required to run .buildkite/steps/build-image.sh locally
 docker pull neonlabsorg/solana:${SOLANA_REVISION}
