0f70d34e384200f6cbbe54e071ca63c6a76f16c6
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-06-02 22:04:55 +0400

    Fix

--- proxy/testing/test_mempool.py
+++ proxy/testing/test_mempool.py
@@ -1,72 +1,55 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import Tuple
+from asyncio import Task
+
 import unittest
-from proxy.mempool.mempool_api import MPTxRequest
-from proxy.mempool.mempool_scheduler import MPNeonTxScheduler
+from unittest.mock import patch, MagicMock
+
+from ..mempool.mempool import MemPool, IMPExecutor
+from ..mempool.mempool_api import NeonTxExecCfg, MPRequest, MPTxRequest
+
+from ..common_neon.eth_proto import Trx as NeonTx
+
+neon_logger = logging.getLogger("neon")
+neon_logger.setLevel(logging.ERROR)
 
 
-class TestTrx:
-    def __init__(self, sender, nonce, gasPrice):
-        self.addr = sender
-        self.nonce = nonce
-        self.gasPrice = gasPrice
+class MockMPExecutor(IMPExecutor):
 
-    def sender(self):
-        return self.addr
+    def submit_mp_request(self, mp_reqeust: MPRequest) -> Tuple[int, Task]:
+        pass
 
+    def is_available(self) -> bool:
+        return True
 
-TEST_DATA=[
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 1, 10)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 1, 15)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 2, 20)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 3, 30)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 3, 40)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 3, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 4, 60)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 5, 70)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 1, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 1, 20)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 2, 30)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 2, 40)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 3, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 4, 60)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 5, 70)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 1, 90)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 2, 30)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 3, 40)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 4, 50)),
-]
-TEST_RESULT=[
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 1, 90)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 1, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 2, 40)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 3, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 4, 60)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("02", 5, 70)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 2, 30)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 3, 40)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("03", 4, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 1, 15)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 2, 20)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 3, 50)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 4, 60)),
-    MPTxRequest(req_id=10, neon_tx=TestTrx("01", 5, 70)),
-]
+    def on_no_liquidity(self, resource_id: int):
+        pass
+
+    def release_resource(self, resource_id: int):
+        pass
 
 
-class Test_Neon_Faucet(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.scheduler = MPNeonTxScheduler()
+class Test(unittest.IsolatedAsyncioTestCase):
 
-    # @unittest.skip("a.i.")
-    def test_01_test_order(self):
-        for req in TEST_DATA:
-            self.scheduler.add_tx(req)
-        for resp in TEST_RESULT:
-            tx_request = self.scheduler.get_tx_for_execution()
-            self.assertEqual(resp.gas_price, tx_request.gas_price)
-            self.assertEqual(resp.nonce, tx_request.nonce)
-            self.assertEqual(resp.address, tx_request.address)
+    async def asyncSetUp(self):
+        self.executor = MockMPExecutor()
+        self.mempool = MemPool(self.executor)
 
+    @patch.object(MockMPExecutor, "submit_mp_request")
+    async def test_single_sender_txs(self, submit_mp_request_mock: MagicMock):
+        neon_tx = NeonTx.fromString(bytearray.fromhex(
+            'f8678080843ade68b194f0dafe87532d4373453b2555c644390e1b99e84c8459682f0080820102a00193e1966a82c5597942370980fb78080901ca86eb3c1b25ec600b2760cfcc94a03efcc1169e161f9a148fd4586e0bcf880648ca74075bfa7a9acc8800614fc9ff'))
 
-if __name__ == '__main__':
-    unittest.main()
+        tx_cfg = NeonTxExecCfg(is_underpriced_tx_without_chainid=False, steps_executed=100)
+        mp_tx_request = MPTxRequest(signature="asdf", neon_tx=neon_tx, neon_tx_exec_cfg=tx_cfg, emulating_result=dict(),
+                                    req_id="test_rq_1")
+        await self.mempool._on_send_tx_request(mp_tx_request)
+        await asyncio.sleep(0)
+        submit_mp_request_mock.assert_called_once()
+        submit_mp_request_mock.assert_called_with(mp_tx_request)
+
+    async def test_test(self):
+        pass
