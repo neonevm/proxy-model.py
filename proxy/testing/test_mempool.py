5dc2bdf1cde01dfd97f313d91a7450a0d952093c
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-06-02 22:05:56 +0400

    Add most simple test

--- proxy/testing/test_mempool.py
+++ proxy/testing/test_mempool.py
@@ -41,15 +41,14 @@ class Test(unittest.IsolatedAsyncioTestCase):
     @patch.object(MockMPExecutor, "submit_mp_request")
     async def test_single_sender_txs(self, submit_mp_request_mock: MagicMock):
         neon_tx = NeonTx.fromString(bytearray.fromhex(
-            'f8678080843ade68b194f0dafe87532d4373453b2555c644390e1b99e84c8459682f0080820102a00193e1966a82c5597942370980fb78080901ca86eb3c1b25ec600b2760cfcc94a03efcc1169e161f9a148fd4586e0bcf880648ca74075bfa7a9acc8800614fc9ff'))
+            'f8678080843ade68b194f0dafe87532d4373453b2555c644390e1b99e84c8459682f0080820102a00193e1966a82c5597942370980'
+            'fb78080901ca86eb3c1b25ec600b2760cfcc94a03efcc1169e161f9a148fd4586e0bcf880648ca74075bfa7a9acc8800614fc9ff'))
 
         tx_cfg = NeonTxExecCfg(is_underpriced_tx_without_chainid=False, steps_executed=100)
-        mp_tx_request = MPTxRequest(signature="asdf", neon_tx=neon_tx, neon_tx_exec_cfg=tx_cfg, emulating_result=dict(),
-                                    req_id="test_rq_1")
+        mp_tx_request = MPTxRequest(req_id="test_rq_1", signature="asdf", neon_tx=neon_tx, neon_tx_exec_cfg=tx_cfg,
+                                    emulating_result=dict())
         await self.mempool._on_send_tx_request(mp_tx_request)
         await asyncio.sleep(0)
         submit_mp_request_mock.assert_called_once()
         submit_mp_request_mock.assert_called_with(mp_tx_request)
 
-    async def test_test(self):
-        pass
