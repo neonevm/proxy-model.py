955399b998c69b00fad69ea1f08e791708948d67
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-06-11 18:26:13 +0300

    Refactor parallel execution of tests. #775

--- proxy/deploy-test.sh
+++ proxy/deploy-test.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 set -xeuo pipefail
 
-echo "Deploy test..."
+if [ "${SKIP_PREPARE_DEPLOY_TEST:-NO}" != "YES" ]; then
+    SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+    source ${SCRIPT_PATH}/prepare-deploy-test.sh
+fi
 
-solana address || solana-keygen new --no-passphrase
-export $(/spl/bin/neon-cli --commitment confirmed --url $SOLANA_URL --evm_loader "$EVM_LOADER" neon-elf-params)
-
-curl -v --header "Content-Type: application/json" --data '{"method":"eth_blockNumber","id":1,"jsonrpc":"2.0","params":[]}' $PROXY_URL
-
-solana config get
-solana address
-solana airdrop 1000
-solana balance
-
-set ${TESTNAME:=*}
-
-export ETH_TOKEN_MINT=$NEON_TOKEN_MINT
-export TEST_PROGRAM=$(solana address -k /spl/bin/proxy_program-keypair.json)
-
-# python3 -m unittest discover -v -p "test_${TESTNAME}.py"
-find . -name "test_${TESTNAME}.py" -printf "%f\n" | sort | parallel --halt now,fail=1 --jobs 4 python3 -m unittest discover -v -p {}
-
+echo "Deploy test ..."
+export $(cat .test-env | xargs)
+python3 -m unittest discover -v -p "${TESTNAME:-test_*.py}"
 echo "Deploy test success"
+
 exit 0
