94a062e209e83507a4987268dc626faea4c0d9de
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-07-29 17:42:18 +0300

    Set version v0.9.6

--- proxy/neon_rpc_api_model/neon_rpc_api_model.py
+++ proxy/neon_rpc_api_model/neon_rpc_api_model.py
@@ -30,7 +30,7 @@ from .transaction_sender import NeonTxSendStrategySelector
 from .operator_resource_list import OperatorResourceList
 from .transaction_validator import NeonTxValidator
 
-NEON_PROXY_PKG_VERSION = '0.9.5'
+NEON_PROXY_PKG_VERSION = '0.9.6'
 NEON_PROXY_REVISION = 'NEON_PROXY_REVISION_TO_BE_REPLACED'
 
 
