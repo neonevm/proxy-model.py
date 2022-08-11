607b404bdbfb19fc3ebb7b71169aae75525f9898
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-08-11 15:12:47 +0300

    NDEV-340 update db schema

--- proxy/db/scheme.sql
+++ proxy/db/scheme.sql
@@ -1,3 +1,44 @@
+    ---- Prepare stage
+    CREATE OR REPLACE FUNCTION does_table_have_column(t_name TEXT, c_name TEXT)
+        RETURNS BOOLEAN
+        LANGUAGE plpgsql
+    AS
+    $$
+    DECLARE
+        column_count INT;
+    BEGIN
+        SELECT COUNT(t.column_name)
+          INTO column_count
+          FROM information_schema.columns AS t
+         WHERE t.table_name=t_name and t.column_name=c_name;
+        RETURN column_count > 0;
+    END;
+    $$;
+
+
+    DO $$
+    DECLARE
+        ----------
+    BEGIN
+        IF does_table_have_column('solana_block', 'slot') THEN
+            ALTER TABLE solana_block RENAME TO oldv1_solana_blocks;
+        END IF;
+
+        IF does_table_have_column('neon_transactions', 'neon_sign') THEN
+            ALTER TABLE neon_transactions RENAME TO oldv1_neon_transactions;
+        END IF;
+
+        IF does_table_have_column('neon_transaction_logs', 'blockNumber') THEN
+            ALTER TABLE neon_transaction_logs RENAME TO oldv1_neon_transaction_logs;
+        END IF;
+
+        IF does_table_have_column('solana_neon_transactions', 'sol_sign') THEN
+            DROP TABLE solana_neon_transactions;
+        END IF;
+    END $$;
+
+    --- Initialize stage
+
     CREATE TABLE IF NOT EXISTS constants (
         key TEXT UNIQUE,
         value BYTEA
@@ -7,8 +48,6 @@
         key TEXT UNIQUE,
         value BYTEA
     );
-
-    DROP TABLE IF EXISTS neon_accounts;
 
     CREATE TABLE IF NOT EXISTS failed_airdrop_attempts (
         attempt_time    BIGINT,
@@ -33,10 +72,9 @@
         is_finalized BOOL,
         is_active BOOL
     );
-    CREATE UNIQUE INDEX IF NOT EXISTS solana_blocks_slot ON solana_blocks(block_slot);
-    CREATE INDEX IF NOT EXISTS solana_blocks_hash ON solana_blocks(block_hash);
-    CREATE INDEX IF NOT EXISTS solana_blocks_slot_active ON solana_blocks(block_slot, is_active);
-
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_solana_blocks_slot ON solana_blocks(block_slot);
+    CREATE INDEX IF NOT EXISTS idx_solana_blocks_hash ON solana_blocks(block_hash);
+    CREATE INDEX IF NOT EXISTS idx_solana_blocks_slot_active ON solana_blocks(block_slot, is_active);
 
     CREATE TABLE IF NOT EXISTS neon_transaction_logs (
         address TEXT,
@@ -52,10 +90,10 @@
 
         topic_list BYTEA
     );
-    CREATE UNIQUE INDEX IF NOT EXISTS neon_transaction_logs_block_tx_log ON neon_transaction_logs(block_slot, tx_hash, tx_log_idx);
-    CREATE INDEX IF NOT EXISTS neon_transaction_logs_address ON neon_transaction_logs(address);
-    CREATE INDEX IF NOT EXISTS neon_transaction_logs_topic ON neon_transaction_logs(topic);
-    CREATE INDEX IF NOt EXISTS neon_transaction_logs_block_slot ON neon_transaction_logs(block_slot);
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_neon_transaction_logs_block_tx_log ON neon_transaction_logs(block_slot, tx_hash, tx_log_idx);
+    CREATE INDEX IF NOT EXISTS idx_neon_transaction_logs_address ON neon_transaction_logs(address);
+    CREATE INDEX IF NOT EXISTS idx_neon_transaction_logs_topic ON neon_transaction_logs(topic);
+    CREATE INDEX IF NOt EXISTS idx_neon_transaction_logs_block_slot ON neon_transaction_logs(block_slot);
 
     CREATE TABLE IF NOT EXISTS solana_neon_transactions (
         sol_sig TEXT,
@@ -72,9 +110,9 @@
         max_bpf_cycle_cnt INT,
         used_bpf_cycle_cnt INT
     );
-    CREATE UNIQUE INDEX IF NOT EXISTS solana_neon_transactions_neon_sol_idx_inner ON solana_neon_transactions(sol_sig, block_slot, idx, inner_idx);
-    CREATE INDEX IF NOT EXISTS solana_neon_transactions_neon_sig ON solana_neon_transactions(neon_sig, block_slot);
-    CREATE INDEX IF NOT EXISTS solana_neon_transactions_neon_block ON solana_neon_transactions(block_slot);
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_solana_neon_transactions_neon_sol_idx_inner ON solana_neon_transactions(sol_sig, block_slot, idx, inner_idx);
+    CREATE INDEX IF NOT EXISTS idx_solana_neon_transactions_neon_sig ON solana_neon_transactions(neon_sig, block_slot);
+    CREATE INDEX IF NOT EXISTS idx_solana_neon_transactions_neon_block ON solana_neon_transactions(block_slot);
 
     CREATE TABLE IF NOT EXISTS neon_transactions (
         neon_sig TEXT,
@@ -106,9 +144,9 @@
         calldata TEXT,
         logs BYTEA
     );
-    CREATE INDEX IF NOT EXISTS neon_transactions_sol_sig_block ON neon_transactions(sol_sig, block_slot);
-    CREATE UNIQUE INDEX IF NOT EXISTS neon_transactions_neon_sig_block ON neon_transactions(neon_sig, block_slot);
-    CREATE INDEX IF NOT EXISTS neon_transactions_block_slot_tx_idx ON neon_transactions(block_slot, tx_idx);
+    CREATE INDEX IF NOT EXISTS idx_neon_transactions_sol_sig_block ON neon_transactions(sol_sig, block_slot);
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_neon_transactions_neon_sig_block ON neon_transactions(neon_sig, block_slot);
+    CREATE INDEX IF NOT EXISTS idx_neon_transactions_block_slot_tx_idx ON neon_transactions(block_slot, tx_idx);
 
     CREATE TABLE IF NOT EXISTS solana_transaction_costs (
         sol_sig TEXT,
@@ -117,12 +155,70 @@
         operator TEXT,
         sol_spent BIGINT
     );
-    CREATE UNIQUE INDEX IF NOT EXISTS solana_transaction_costs_sig ON solana_transaction_costs(sol_sig, block_slot);
-    CREATE INDEX IF NOT EXISTS solana_transaction_costs_slot ON solana_transaction_costs(block_slot);
-    CREATE INDEX IF NOT EXISTS solana_transaction_costs_operator ON solana_transaction_costs(operator, block_slot);
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_solana_transaction_costs_sig ON solana_transaction_costs(sol_sig, block_slot);
+    CREATE INDEX IF NOT EXISTS idx_solana_transaction_costs_slot ON solana_transaction_costs(block_slot);
+    CREATE INDEX IF NOT EXISTS idx_solana_transaction_costs_operator ON solana_transaction_costs(operator, block_slot);
 
     CREATE TABLE IF NOT EXISTS solana_transaction_signatures (
         block_slot  BIGINT,
         signature   TEXT
     );
-    CREATE UNIQUE INDEX IF NOT EXISTS solana_transaction_signatures_sign ON solana_transaction_signatures(block_slot);
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_solana_transaction_signatures_sig ON solana_transaction_signatures(block_slot);
+
+    ---- Upgrade stage
+    DO $$
+    DECLARE
+        ----------
+    BEGIN
+        IF does_table_have_column('oldv1_solana_blocks', 'slot') THEN
+            INSERT INTO solana_blocks(
+                block_slot, block_hash, block_time,
+                parent_block_slot, is_finalized, is_active)
+            SELECT
+                slot, hash, blocktime,
+                0, TRUE, TRUE
+            FROM oldv1_solana_blocks;
+
+            DROP TABLE oldv1_solana_blocks;
+        END IF;
+
+        IF does_table_have_column('oldv1_neon_transactions', 'neon_sign') THEN
+            INSERT INTO neon_transactions(
+                neon_sig, from_addr,
+                sol_sig, sol_ix_idx, sol_ix_inner_idx,
+                block_slot, tx_idx,
+                nonce, gas_price, gas_limit, value, to_addr, contract, calldata,
+                gas_used, status, return_value, logs,
+                v, r, s
+            )
+            SELECT
+                neon_sign, from_addr,
+                sol_sign, idx, 0,
+                slot, 0,
+                nonce, gas_price, gas_limit, value, to_addr, contract, calldata,
+                gas_used, status, return_value, logs,
+                v, r, s
+            FROM oldv1_neon_transactions;
+
+            DROP TABLE oldv1_neon_transactions;
+        END IF;
+
+        IF does_table_have_column('oldv1_neon_transaction_logs', 'blockNumber') THEN
+            INSERT INTO neon_transaction_logs(
+                address,
+                block_slot, tx_hash, tx_idx, tx_log_idx, log_idx,
+                topic, log_data, topic_list
+            )
+            SELECT
+                address,
+                blockNumber, transactionHash, 0, transactionLogIndex, 0,
+                topic, '', ''
+            FROM oldv1_neon_transaction_logs;
+
+            DROP TABLE oldv1_neon_transaction_logs;
+        END IF;
+    END $$;
+
+    DROP TABLE IF EXISTS solana_neon_transactions_costs;
+    DROP TABLE IF EXISTS solana_transaction_receipts;
+    DROP TABLE IF EXISTS test_storage;
