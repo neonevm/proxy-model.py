e8651cfe3032c709c8f63275491ebf301b8bf93b
Denis Avvakumov <denisavvakumov@gmail.com>
2022-08-22 07:32:14 +0300

    NDEV-552 Use proxy dir to build fuzzer

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,7 @@
 python -c "import sys; print(sys.path)"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
+cd proxy
 for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
   compile_python_fuzzer $fuzzer
 done
---
+++ proxy/fuzz_ethbalance.py
@@ -0,0 +1,1 @@
+import proxy.testing._fuzz_api
