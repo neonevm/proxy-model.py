c65e54da7dc09ff991886516cd2fcb393240e881
Denis Avvakumov <denisavvakumov@gmail.com>
2022-08-23 15:47:30 +0300

    NDEV-552 Fix typo in Dockerfile

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,9 @@
 python -c "import sys; print(sys.path)"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-cd proxy
+ls -l $SRC
+ls -l $SRC/neon_py
+cd $SRC
 for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
   compile_python_fuzzer $fuzzer
 done
