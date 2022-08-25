38110300b67848bc8549c94dfbffa853ed533971
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-08-25 15:04:13 +0300

    Add background service (ndev-522-check-evm-version)

---
+++ proxy/background/background_service.py
@@ -0,0 +1,22 @@
+from logged_groups import logged_group
+import asyncio
+from multiprocessing import Process
+
+@logged_group("neon.Background")
+class BackgroundService:
+
+    def __init__(self, config: IConfig):
+        self.event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.event_loop)
+        self._process = Process(target=self.run)
+        self._config = config
+
+    def start(self):
+        self.info("Run background process")
+        self._process.start()
+
+    def run(self):
+        try:
+            self.event_loop.run_forever()
+        except Exception as err:
+            self.error(f"Failed to run background_service: {err}")
--- proxy/neon_proxy_app.py
+++ proxy/neon_proxy_app.py
@@ -1,5 +1,6 @@
 from .proxy import entry_point
 from .mempool.mempool_service import MPService
+from .background.background_service import BackgroundService
 
 from .statistics_exporter.prometheus_proxy_server import PrometheusProxyServer
 from .common_neon.config import Config
@@ -10,8 +11,10 @@ class NeonProxyApp:
     def __init__(self):
         self._config = Config()
         self._mempool_service = MPService(self._config)
+        self._background_service = BackgroundService(self._config)
 
     def start(self):
+        self._background_service.start()
         self._mempool_service.start()
         PrometheusProxyServer()
         entry_point()
