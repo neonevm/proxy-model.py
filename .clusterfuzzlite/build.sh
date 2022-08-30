c5126462b3aa9046d428be73bdef3c98b1be03c4
Denis Avvakumov <denisavvakumov@gmail.com>
2022-08-30 12:17:15 +0300

    NDEV-552 Print HEAD in build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,7 +2,7 @@ python -c "import sys; print(sys.path)"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 ls -l $SRC
-ls -l $SRC/neon_py
+git rev-parse HEAD
 cd $SRC
 for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
   compile_python_fuzzer $fuzzer
