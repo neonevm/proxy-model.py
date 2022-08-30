0c91c4b17c3af6923b64c9ab3e6078f674ebb3c1
Denis Avvakumov <denisavvakumov@gmail.com>
2022-08-30 09:44:37 +0300

    NDEV-552 Update workflow and Dockerfile

--- proxy/mempool/mempool_service.py
+++ proxy/mempool/mempool_service.py
@@ -1,3 +1,4 @@
+import traceback
 from logged_groups import logged_group
 import asyncio
 from multiprocessing import Process
@@ -75,7 +76,8 @@ class MPService(IPickableDataServerUser, IMPExecutorMngUser):
             self._mempool = MemPool(self._config, self._operator_resource_mng, self._mp_executor_mng)
             self.event_loop.run_forever()
         except Exception as err:
-            self.error(f"Failed to run mempool_service: {err}")
+            err_tb = "".join(traceback.format_tb(err.__traceback__))
+            self.error(f"Failed to run mempool_service:  Error: {err}, Traceback: {err_tb}")
 
     def on_resource_released(self, resource_id: int):
         self._mempool.on_resource_got_available(resource_id)
