6c0729f92c141f803ac988c2b29305eb78aa2afe
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-09-01 10:45:32 +0300

    Remove unneeded background service (ndev-522-check-evm-version)

--- proxy/neon_proxy_app.py
+++ proxy/neon_proxy_app.py
@@ -1,6 +1,5 @@
 from .proxy import entry_point
 from .mempool.mempool_service import MPService
-from .background.background_service import BackgroundService
 
 from .statistics_exporter.prometheus_proxy_server import PrometheusProxyServer
 from .common_neon.config import Config
@@ -11,10 +10,8 @@ class NeonProxyApp:
     def __init__(self):
         self._config = Config()
         self._mempool_service = MPService(self._config)
-        self._background_service = BackgroundService(self._config)
 
     def start(self):
-        self._background_service.start()
         self._mempool_service.start()
         PrometheusProxyServer()
         entry_point()
