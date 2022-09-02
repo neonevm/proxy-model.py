843cd0cfdb187396cec397b06ce22d62d29bacb9
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-09-02 13:52:50 +0300

    Try increase NEON_CLI_TIMEOUT to 1 sec (ndev-522-check-evm-version)

--- proxy/common_neon/environment_data.py
+++ proxy/common_neon/environment_data.py
@@ -5,7 +5,7 @@ from solana.publickey import PublicKey
 SOLANA_URL = os.environ.get("SOLANA_URL", "http://localhost:8899")
 PP_SOLANA_URL = os.environ.get("PP_SOLANA_URL", SOLANA_URL)
 EVM_LOADER_ID = os.environ.get("EVM_LOADER")
-neon_cli_timeout = float(os.environ.get("NEON_CLI_TIMEOUT", "0.5"))
+neon_cli_timeout = float(os.environ.get("NEON_CLI_TIMEOUT", "1"))
 CONFIRMATION_CHECK_DELAY = float(os.environ.get("NEON_CONFIRMATION_CHECK_DELAY", "0.1"))
 CONTINUE_COUNT_FACTOR = int(os.environ.get("CONTINUE_COUNT_FACTOR", "3"))
 TIMEOUT_TO_RELOAD_NEON_CONFIG = int(os.environ.get("TIMEOUT_TO_RELOAD_NEON_CONFIG", "3600"))
