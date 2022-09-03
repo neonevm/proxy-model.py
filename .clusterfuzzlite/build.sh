8161d69a1727c5864cad5f20627f2ebf98ced234
Denis Avvakumov <denisavvakumov@gmail.com>
2022-09-03 06:54:39 +0300

    NDEV-552: Add container based on clusterfuzzlite-run-fuzzers

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,7 @@
 python -c "import sys; print(sys.path)"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
+yes | cp -i log_cfg_fuzzing.json log_cfg.json
 ls -l $SRC
 git rev-parse HEAD
 cd $SRC
