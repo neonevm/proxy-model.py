e21cc34710e158f856548d2be2074bed993e26f6
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-09-06 16:28:51 +0300

    Set uniq start for mempool service

--- proxy/testing/_base_fuzz_config.py
+++ proxy/testing/_base_fuzz_config.py
@@ -2,6 +2,8 @@ import os
 
 from proxy.common_neon.config import Config
 
+os.environ["PERM_ACCOUNT_START"] = str(os.getpid())
+
 
 class FuzzConfig(Config):
     def get_perm_account_start(self) -> int:
