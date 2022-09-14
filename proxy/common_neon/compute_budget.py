0250293e2ab4543e92d67712b4356638cf5c955a
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-09-14 18:23:44 +0300

    NDEV-589 add support of new ComputeBudget ix

--- proxy/common_neon/compute_budget.py
+++ proxy/common_neon/compute_budget.py
@@ -10,19 +10,27 @@ from typing import Union
 
 class ComputeBudget:
     @staticmethod
-    def requestUnits(units, additional_fee) -> TransactionInstruction:
-        return TransactionInstruction(
-            program_id=COMPUTE_BUDGET_ID,
-            keys=[],
-            data=bytes.fromhex("00") + units.to_bytes(4, "little") + additional_fee.to_bytes(4, "little")
-        )
-
-    @staticmethod
-    def requestHeapFrame(heap_frame) -> TransactionInstruction:
+    def requestHeapFrame(heap_frame: int) -> TransactionInstruction:
         return TransactionInstruction(
             program_id=COMPUTE_BUDGET_ID,
             keys=[],
             data=bytes.fromhex("01") + heap_frame.to_bytes(4, "little")
+        )
+
+    @staticmethod
+    def requestUnits(units: int) -> TransactionInstruction:
+        return TransactionInstruction(
+            program_id=COMPUTE_BUDGET_ID,
+            keys=[],
+            data=bytes.fromhex("02") + units.to_bytes(4, "little")
+        )
+
+    @staticmethod
+    def setAdditionalFee(additional_fee: int) -> TransactionInstruction:
+        return TransactionInstruction(
+            program_id=COMPUTE_BUDGET_ID,
+            keys=[],
+            data=bytes.fromhex("03") + additional_fee.to_bytes(8, "little")
         )
 
 
@@ -30,10 +38,11 @@ class TransactionWithComputeBudget(Transaction):
     def __init__(self, compute_units: Optional[int] = None, *args, **kwargs):
         Transaction.__init__(self, *args, **kwargs)
         compute_units = compute_units if compute_units is not None else ElfParams().neon_compute_units
-        additional_fee = ElfParams().neon_additional_fee
-        self.instructions.append(ComputeBudget.requestUnits(compute_units, additional_fee))
+        self.instructions.append(ComputeBudget.requestUnits(compute_units))
         heap_frame = ElfParams().neon_heap_frame
         self.instructions.append(ComputeBudget.requestHeapFrame(heap_frame))
+        # additional_fee = ElfParams().neon_additional_fee
+        # self.instructions.append(ComputeBudget.setAdditionalFee(additional_fee))
 
     def add(self, *args: Union[Transaction, TransactionInstruction]) -> TransactionWithComputeBudget:
         """Add one or more instructions to this Transaction."""
