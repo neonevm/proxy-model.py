81698e5f8235f21a455f94141dfc9eea76e89c87
Andrey Falaleev <andrew.falaleev@gmail.com>
2022-10-18 11:33:30 +0400

    Set version to v0.12.3

--- proxy/neon_rpc_api_model/neon_rpc_api_worker.py
+++ proxy/neon_rpc_api_model/neon_rpc_api_worker.py
@@ -32,7 +32,7 @@ from ..mempool import MemPoolClient, MP_SERVICE_ADDR, MPTxSendResult, MPTxSendRe
 from ..statistics_exporter.proxy_metrics_interface import StatisticsExporter
 
 
-NEON_PROXY_PKG_VERSION = '0.12.2'
+NEON_PROXY_PKG_VERSION = '0.12.3'
 NEON_PROXY_REVISION = 'NEON_PROXY_REVISION_TO_BE_REPLACED'
 
 
