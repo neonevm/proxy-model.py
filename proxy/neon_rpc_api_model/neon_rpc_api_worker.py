8faa361ce16a9ea09166bfb72cc8bc045f47c29a
Anton Lisanin <lisanin.anton@gmail.com>
2023-07-05 11:39:45 +0300

    Set version to 0.16-dev (#90)

--- proxy/neon_rpc_api_model/neon_rpc_api_worker.py
+++ proxy/neon_rpc_api_model/neon_rpc_api_worker.py
@@ -36,7 +36,7 @@ from .nonce_validator import NeonTxNonceValidator
 from .transaction_validator import NeonTxValidator
 
 
-NEON_PROXY_PKG_VERSION = '0.15.0-dev'
+NEON_PROXY_PKG_VERSION = '0.16.0-dev'
 NEON_PROXY_REVISION = 'NEON_PROXY_REVISION_TO_BE_REPLACED'
 LOG = logging.getLogger(__name__)
 
