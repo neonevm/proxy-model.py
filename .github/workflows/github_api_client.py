ba0c8d84b651ef476f28077fd8fa7918714cd854
Kristina Nikolaeva <112946046+kristinaNikolaevaa@users.noreply.github.com>
2023-09-25 09:50:34 +0200

    Ndev 2244 all dapps tests (#150)
    * added new label to run all tests
    
    * Update pipeline.yml
    
    * Update pipeline.yml
    
    * Update pipeline.yml
    
    * Update pipeline.yml
    
    * Update pipeline.yml

--- .github/workflows/github_api_client.py
+++ .github/workflows/github_api_client.py
@@ -23,22 +23,22 @@ class GithubClient():
             f"{NEON_TESTS_ENDPOINT}/actions/workflows/dapps.yml/runs?branch={branch}", headers=self.headers)
         return int(response.json()["total_count"])
 
-    def run_dapps_dispatches(self, proxy_url, solana_url, faucet_url, pr_url_for_report, network_id='111',
+    def run_dapps_dispatches(self, proxy_url, solana_url, faucet_url, pr_url_for_report, test_set, network_id='111',
                              branch='develop'):
-
+        dapps = "aave,saddle,uniswap-v3" if test_set=='fullTestSuite' else ''
         data = {"ref": branch,
                 "inputs": {"proxy_url": proxy_url,
                            "solana_url": solana_url,
                            "faucet_url": faucet_url,
                            "network_id": network_id,
                            "pr_url_for_report": pr_url_for_report,
-                           "dapps": "aave,saddle,uniswap-v3",
+                           "dapps": dapps,
                            "network": "custom"}
                 }
-        response = requests.post(
-            f"{NEON_TESTS_ENDPOINT}/actions/workflows/dapps.yml/dispatches", json=data, headers=self.headers)
         click.echo(f"Sent data: {data}")
         click.echo(f"Headers: {self.headers}")
+        response = requests.post(
+            f"{NEON_TESTS_ENDPOINT}/actions/workflows/dapps.yml/dispatches", json=data, headers=self.headers)
         click.echo(f"Status code: {response.status_code}")
         if response.status_code != 204:
             raise RuntimeError(f"proxy-model.py action is not triggered. {response.text}")
