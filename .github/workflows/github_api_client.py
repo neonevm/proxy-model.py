be20bebe817d9be96bce4558eee3dda31ece9712
Kristina Nikolaeva <112946046+kristinaNikolaevaa@users.noreply.github.com>
2023-11-03 18:01:33 +0100

    fix dapps list for CI (#182)
    * fix NETWORK_NAME
    
    * Revert "fix NETWORK_NAME"
    
    This reverts commit 5a7e555f8960a7b3902310943436505a98db02d2.
    
    * fix dapps  list

--- .github/workflows/github_api_client.py
+++ .github/workflows/github_api_client.py
@@ -25,7 +25,7 @@ class GithubClient():
 
     def run_dapps_dispatches(self, proxy_url, solana_url, faucet_url, pr_url_for_report, test_set, network_id='111',
                              branch='develop'):
-        dapps = "aave,saddle,uniswap-v3" if test_set=='fullTestSuite' else ''
+        dapps = "" if test_set=='extendedFullTestSuite' else "aave,saddle,uniswap-v3"
         data = {"ref": branch,
                 "inputs": {"proxy_url": proxy_url,
                            "solana_url": solana_url,
